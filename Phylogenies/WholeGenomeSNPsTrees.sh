0c64d89a9bad967f2d5447f104c51d432875a866
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-04-20 18:21:30 -0300

    Update WholeGenomeSNPsTrees.sh

--- Phylogenies/WholeGenomeSNPsTrees.sh
+++ Phylogenies/WholeGenomeSNPsTrees.sh
@@ -5,7 +5,7 @@
 python vcf2phylip.py --input ../vcffiles/Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.vcf.gz --output-prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8
 
 #2. # IQ-TREE generation: 
-iqtree -s Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy -o Ccar_01 -b 100 --prefix modified_snps.min4_Lepis_RefCcar_100boots
+iqtree -s Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --alrt 1000 -B 1000 -b 100 -o Snovacapitalis152_lin --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.100boots_UFb_ALR -T 5
 
 #3. run RAxML-NG
 raxml-ng-mpi --msa Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --all --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8 --threads 10 --model GTR --outgroup Ccar_01 --bs-trees 100 --redo
