750730ee5bc19f36fea1d0d94a29626df2787f82
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-04-23 10:08:14 -0300

    Update WholeGenomeSNPsTrees.sh

--- Phylogenies/WholeGenomeSNPsTrees.sh
+++ Phylogenies/WholeGenomeSNPsTrees.sh
@@ -4,8 +4,19 @@
 # https://github.com/edgardomortiz/vcf2phylip
 python vcf2phylip.py --input ../vcffiles/Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.vcf.gz --output-prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8
 
-#2. # IQ-TREE generation: 
+# IQ-TREE: 
 iqtree -s Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --alrt 1000 -B 1000 -b 100 -o Snovacapitalis152_lin --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.100boots_UFb_ALR -T 5
 
-#3. run RAxML-NG
+# RAxML-NG:
 raxml-ng-mpi --msa Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --all --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8 --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
+
+# NJ with VCF2Dis
+# https://github.com/hewm2008/VCF2Dis
+# conda activate R 
+  # 1 run
+../../Softwares/VCF2Dis-1.54/bin/VCF2Dis -i ../vcffiles/FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz -o allspecies_project1_2_5_6_7_8 -TreeMethod 1
+  # 2 run
+../../Softwares/VCF2Dis-1.54/bin/VCF2Dis -i ../vcffiles/FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz -o Second2_allspecies_project1_2_5_6_7_8_DistanceMatrix -TreeMethod 1 -OutPut p_dist.matrix
+
+# CASTER:
+../../Softwares/ASTER/bin/caster-site --output CASTER --thread 5 --support 1 --input Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy 2> CASTER.log
