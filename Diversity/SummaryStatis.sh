c3dcf2fa962182e5822bbad8c784a42b5ca868e4
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-05 15:42:45 -0300

    Update and rename SummaryStatis to SummaryStatis.sh

--- Diversity/SummaryStatis.sh
+++ Diversity/SummaryStatis.sh
@@ -7,6 +7,8 @@ vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_merged
 
 # het 
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz --hardy --out HETstats_OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8
+# het per individual
+bcftools stats -s - ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz > HetOnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.txt
 
 # inbreeding coefficient
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz --het --out Fstats_OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8
