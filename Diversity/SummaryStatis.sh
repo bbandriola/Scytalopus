5a2ab153d6e520ce5ab540bfca13b05a30616d10
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-07 07:51:01 -0300

    Update SummaryStatis.sh

--- Diversity/SummaryStatis.sh
+++ Diversity/SummaryStatis.sh
@@ -1,30 +1,6 @@
 # missing per ind:
 vcftools --gzvcf OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz --missing-indv --out OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8
 
-# densidade de SNVs
-# definition: Allelic richness was defined as the number of alleles at a locus
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz --SNPdensity 100000 --out SNVdensity_OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8
-  # to calculate the SNP density through all samples, do a loop to generate independents output from each sample in the VCF file
-  # following Henrique's tutorial: https://github.com/henriquevf/Tutorials/blob/main/SNP%20density%20tutorial.md
-  # 1. generate density per ind
-  #!/bin/bash
-  echo "activate conda"
-  conda activate bcftools
-  echo "starting analyses"
-  vcf_file="../../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz"
-  # Get the base name for output files
-  name=$(basename $vcf_file)
-  name=${name%.vcf.gz}
-  # First get the list of samples from the VCF file
-  samples=$(bcftools query -l $vcf_file)
-  # Iterate over each sample
-  for sample in $samples; do
-      # Process each sample individually
-      vcftools --gzvcf $vcf_file --indv $sample --SNPdensity 1000000 --out ${sample}_SNPsDensity
-  done
-  echo "done"
-  # 2. 
-
 # het 
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz --hardy --out HETstats_OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8
 # het per individual
