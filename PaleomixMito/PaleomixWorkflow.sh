bb405f151d07111e8ab1fcd9024ab8b105ecfd5a
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-15 15:05:04 -0300

    Update PaleomixWorkflow.sh

--- PaleomixMito/PaleomixWorkflow.sh
+++ PaleomixMito/PaleomixWorkflow.sh
@@ -1,15 +1,12 @@
 # workflow to run Paleomix software for mitochondrial genome
 
 # create the Makefile.yalm 
-# 1. rodar o dry run para checar se esta tudo okay com o makefile
+# 1 -> rodar o dry run para checar se esta tudo okay com o makefile
 #conda activate paleomix
 paleomix bam dryrun --max-threads 10 --adapterremoval-max-threads 10 --bwa-max-threads 10 --jre-option=-Xmx16g --temp-root ./temp --destination ./ Makefile
 
-# 2. rodar o paleomix
+# 2 -> run paleomix
 paleomix bam run --max-threads 10 --adapterremoval-max-threads 10 --bwa-max-threads 10 --jre-option=-Xmx16g --temp-root ./temp --destination ./ Makefile
 
-# 3. gerar sequências Consensus
-angsd -i infile.bam -doFasta 2 -doCounts 1 -explode 1 -setMinDepth 5 -p 10 -minQ 30 -out output_prefix
-
-# 4. gerar fasta mascarado
-RepeatMasker -specie testudines -dir masked_fastas/ *.fa
+# 3 -> consensus sequence
+for i in *bam; do angsd -i $i -doFasta 2 -doCounts 1 -explode 1 -setMinDepth 5 -p 10 -minQ 30 -out $i; done
