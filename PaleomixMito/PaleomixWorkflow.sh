e90b2d6914cb01827510d0b7c1c3ca8af7493225
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-16 15:38:26 -0300

    Update PaleomixWorkflow.sh

--- PaleomixMito/PaleomixWorkflow.sh
+++ PaleomixMito/PaleomixWorkflow.sh
@@ -3,10 +3,10 @@
 # create the Makefile.yalm 
 # 1 -> rodar o dry run para checar se esta tudo okay com o makefile
 #conda activate paleomix
-paleomix bam dryrun --max-threads 10 --adapterremoval-max-threads 10 --bwa-max-threads 10 --jre-option=-Xmx16g --temp-root ./temp --destination ./ Makefile
+paleomix bam dryrun --max-threads 5 --adapterremoval-max-threads 5 --bwa-max-threads 5 --jre-option=-Xmx10g --temp-root ./temp --destination ./ Makefile
 
 # 2 -> run paleomix
-paleomix bam run --max-threads 10 --adapterremoval-max-threads 10 --bwa-max-threads 10 --jre-option=-Xmx16g --temp-root ./temp --destination ./ Makefile
+paleomix bam run --max-threads 5 --adapterremoval-max-threads 5 --bwa-max-threads 5 --jre-option=-Xmx10g --temp-root ./temp --destination ./ Makefile
 
 # 3 -> consensus sequence
 for i in *bam; do angsd -i $i -doFasta 2 -doCounts 1 -explode 1 -setMinDepth 5 -p 10 -minQ 30 -out $i; done
