246a6da139cc3df0a4b73b4e274162c54c38a368
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-22 13:02:38 -0300

    Update PCA_ANGSD.sh

--- PCA&Admixture/PCA_ANGSD.sh
+++ PCA&Admixture/PCA_ANGSD.sh
@@ -1,4 +1,5 @@
 # Commands to run PCA with angsd based on genotype likelihoods 
+# based on https://www.popgen.dk/software/index.php/PCAngsd
 
 # 1-> Generate genotype likelihoods
 angsd -b bamlist.txt -ref /media/labgenoma4/DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fna.gz -out allsamples_IndMind5_Maxdepth100 -setMinDepthInd 5 -setMaxDepthInd 100 -GL 2 -nThreads 2 -doGlf 2 -doMajorMinor 1 -doMaf 1 -only_proper_pairs 1 -remove_bads 1 -uniqueOnly 1 -C15 -setMinDepth 100 -setMaxDepth 3000 -SNP_pval 1e-3c -doCounts 1
