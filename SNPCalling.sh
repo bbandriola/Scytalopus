1fad4c828d76ae2c9d616a2af6755af32f95331e
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-22 11:03:07 -0300

    Update SNPCalling.sh

--- SNPCalling.sh
+++ SNPCalling.sh
@@ -24,16 +24,16 @@ bcftools view -s ^EleoindigoticusUCE,EleopsychopompusUCE1,Sdiamantinensis,Siraie
   # result: 16,897,926 sites
     ## South 
     bcftools view -s HCentroLesteSC_lin7,BoaEsperanca1_lin6,NortePR1_lin7,DevonianaPR2_lin5,HNortePR_lin7,ExtremoSulRS2_lin7,CentroOesteSC1_lin7,CentroOesteSC2_lin7,CentroOesteSC3_lin7,CentroOesteSC4_lin7,CentroOesteSC5_lin7,NortePR2_lin7,DevonianaPR3_lin5,DevonianaPR4_lin5,NortePR3_lin7,CentroLesteSC1_lin7,DevonianaPR8_lin5,DevonianaSP3_lin5,DevonianaSP4_lin5,ExtremoSulSC1_lin7,DevonianaSP1_lin5,DevonianaSP2_lin5,BoaEsperanca2_lin6,BoaEsperanca3_lin7,NortePR4_lin7,NortePR5_lin7,CentroLestePR1_lin7,DevonianaPR5_lin5,DevonianaPR6_lin5,DevonianaPR7_lin5,ExtremoSulRS3_lin7 -Oz -o SouthcladeSspeluncae_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz OnlySspeluncae_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz
-    # results:
+    # results: 16,897,926
     ## North 
     bcftools view -s SulMantiqueira1_lin3,SulMantiqueira2_lin3,SulMantiqueira3_lin3,SulMantiqueira5_lin3,SerradaLontras2_lin1,SerradaLontras3_lin1,SerraDosOrgaos1_lin2,SerraDosOrgaos2_lin2,NorteMantiqueira1_lin3,SulMantiqueira4_lin3,NorteMantiqueira3_lin3,NorteMantiqueira4_lin3,SerraDosOrgaos3_lin2,Caparao_lin3,SerraDaOuricana2_lin1,SerraDaOuricana3_lin1,SerraDaOuricana4_lin1,SerradaLontras4_lin1,NorteMantiqueira2_lin3,SerraDaOuricana1_lin1,SerradaLontras1_lin1,SerradaLontras5_lin1 -Oz -o NorthcladeSspeluncae_GeographicNames__FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.recode.vcf.gz ../GeographicNamesFilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz
     # results: 
       ## North Lin 2 e 3 
       bcftools view -s SulMantiqueira2_lin3,SulMantiqueira3_lin3,SulMantiqueira5_lin3,NorteMantiqueira1_lin3,SulMantiqueira4_lin3,NorteMantiqueira3_lin3,NorteMantiqueira4_lin3,SerraDosOrgaos3_lin2,Caparao_lin3,NorteMantiqueira2_lin3 -Oz -o Lin2Lin3Sspeluncae_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz OnlySspeluncae_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz
-      # results:
-  ## Lineage 4
-  bcftools view -s CunhaSerraDoMarRJ1_lin4,BocainaSerraDoMocainaSerraDoMarRJ3_lin4,BocainaSerraDoMarRJ4_lin4,CunhaSerraDoMarRJ2_lin4 -Oz -o Lin4_GeographicNames_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz ../GeographicNamesFilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz
-  # results:
+      # results: 16,897,926
+    ## Lineage 4
+    bcftools view -s CunhaSerraDoMarRJ1_lin4,BocainaSerraDoMocainaSerraDoMarRJ3_lin4,BocainaSerraDoMarRJ4_lin4,CunhaSerraDoMarRJ2_lin4 -Oz -o Lin4_GeographicNames_FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz ../GeographicNamesFilteredMax30missingDepthmin5_mergedVCFproject0_1_2_4_5_6_7_8.vcf.gz
+    # results:
 
 # 4. Thin independent SNPs for Admixture
   # all speluncae filtered PCA
