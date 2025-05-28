14119b3df6519c18a2566d0b78516ea9386cd0c8
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-05-28 20:47:18 -0300

    Update FstDxyPi.sh

--- Diversity/FstDxyPi.sh
+++ Diversity/FstDxyPi.sh
@@ -24,6 +24,17 @@ vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_merged
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin5.txt --weir-fst-pop lin6.txt --out fstlin5_lin6
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin5.txt --weir-fst-pop lin7.txt --out fstlin5_lin7 
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin6.txt --weir-fst-pop lin7.txt --out fstlin6_lin7
+# Fst with pixy according hudson method (ref:https://www.biorxiv.org/content/10.1101/2024.09.24.614506v1.full)
+pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations AllSspeluncaePops.txt --window_size 100000 --n_cores 4 --output_prefix AllSspeluncaePops_WithoutLin1_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  # between populations according PCA
+  pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations Lin3A3B.txt --window_size 100000 --n_cores 1 --output_prefix Lin3A3B_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations Lin3A3B.txt --window_size 100000 --n_cores 1 --output_prefix Lin3A3B_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations Lin67Lin7.txt --window_size 100000 --n_cores 1 --output_prefix Lin67Lin7_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations Lin5Lin7.txt --window_size 100000 --n_cores 1 --output_prefix Lin5Lin7_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations Lin5Lin67.txt --window_size 100000 --n_cores 1 --output_prefix Lin5Lin67_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
+  
+# Fst with pixy according WC method
+pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations AllSspeluncaePops.txt --window_size 100000 --n_cores 2 --output_prefix AllSspeluncaePops_WithoutLin1_FstWC_pixy --bypass_invariant_check 'yes'
 
 # Pi with vcftools
 vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --window-pi 100000 --out pi_100kb_lin1 --keep lin1.txt
