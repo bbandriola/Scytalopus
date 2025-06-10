00c154fa411033e56e20ed813cb36eb21897d551
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-06-10 14:19:59 -0300

    Create RaxmlWindows.sh

---
+++ Phylogenies/RaxmlWindows.sh
@@ -0,0 +1,7 @@
+# 5kb 
+# 1. 
+
+# 2.
+
+# 3. Species tree with Astral
+java -jar ../../../../Softwares/astral/ASTRAL/astral.5.7.8.jar -i allbestTrees_5kb.fa --output 5kb_int100kb.allspecies.tree --branch-annotate 2 --outgroup Ssuperciliariswin5kb
