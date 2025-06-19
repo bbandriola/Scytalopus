3a1ca3da34d3780321ecd3b180f7021da09944cd
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-06-19 16:30:30 -0300

    Update PSMCWorkflow.sh

--- Demography/PSMCWorkflow.sh
+++ Demography/PSMCWorkflow.sh
@@ -1,1 +1,23 @@
-
+# generate diploid fasta
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sdiamantinensis127_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d7 -D70 | gzip > Sdiamantinensis.d7.D70.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Snovacapitalis152_lin_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d10 -D90 | gzip > Snovacapitalis.d10.D90.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Spachecoi154_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d10 -D90 | gzip > Spachecoi.d10.D90.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae106_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > CnetroLestePR1_lin7.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae113_lin3_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d6 -D60 | gzip > NorteMantiqueira3_lin3.d6.D60.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae116_lin3_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D50 | gzip > Caparao_lin3.d5.D50.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae118_lin1_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > SerradaOuricana_lin1.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae120_lin1_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d6 -D60 | gzip > SerradasLontra4_lin1.d6.D60.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae122_lin6_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d10 -D90 | gzip > BoaEsperanca_lin67.d10.D90.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae123_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d10 -D90 | gzip > BoaEsperanca_lin67.d10.D90.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae140_lin5_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d10 -D90 | gzip > DevonianaPR8.d10.D90.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae14_lin3_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d7 -D66 | gzip > SulMantiqueira3_lin3.d7.D66.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae16_lin4_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > CunhaSerradoMarRJ2_lin4.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae2_lin4_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > BocainaSerradoMarRJ1_lin4.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae30_lin5_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > DevonianaPR3_lin5.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae33_lin_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D50 | gzip > Spetrophilus.d5.D50.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae64_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d6 -D60 | gzip > HNortePR_lin7.d6.D60.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae68_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > CentroOesteSC1_lin7.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae83_lin2_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d3 -D30 | gzip > SerradosOrgaos3_lin2.d3.D30.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae87_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d6 -D60 | gzip > NortePR3_lin5.d6.D60.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Sspeluncae95_lin7_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d5 -D45 | gzip > ExtremoSulRS3_lin7.d5.D45.diploid.fq.gz
+bcftools mpileup -Ou -f GCA_013400415.1_ASM1340041v1_genomic.fa Ssuperciliaris_final.bam | bcftools call -c | vcfutils.pl vcf2fq -d3 -D30 | gzip > Ssuperciliaris.d3.D30.diploid.fq.gz
