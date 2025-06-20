9ae572d367eb9f3129d53b8c80856c9a771db412
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-06-20 11:05:21 -0300

    Create 11_3Seq.sh

---
+++ GenomicWindows/11_3Seq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#run 3 seq
+for i in edited*.fa; do /media/labgenoma4/DATAPART6/bandriola/Softwares/3Seq/3seq\ build\ 170612/3seq -f $i -id $i; done
+
+echo "3seq done"
+
+#moving files
+
+mkdir 3seq
+
+mv *.3s.log ./3seq/
+mv *.3s.pvalHist ./3seq/
+mv *.3s.longRec ./3seq/
+mv *.3s.rec ./3seq/
+
+echo "moved files"
+
+#Gerar arquivos com a correção Bonferroni
+
+cd ./3seq/
+
+for i in *.3s.log; do grep -H 'Bonferroni' $i >> Bonferroni_5kb.csv; done
+
+#open the file on excel and check for recombination into the window <cut at 0,01>
+
+echo "done"
