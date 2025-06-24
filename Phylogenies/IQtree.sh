7a45c40e4001a84577dfaf59916ec40266baeb4d
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-06-24 19:02:45 -0300

    Create IQtree.sh

---
+++ Phylogenies/IQtree.sh
@@ -0,0 +1,7 @@
+# IQtree
+
+# 1. vcf to phy 
+python vcf2phylip/vcf2phylip.py -i FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz
+
+# 2. IQ-TREE
+iqtree -s FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz.min4.phy -o Ssuperciliaris -T 5 -b 100 --prefix allsamples_FilteredPCA_FilteredMax30missingDepthmin5_100
