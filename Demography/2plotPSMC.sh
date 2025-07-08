8097436c4da9456cf69e494e8c1bbb0b9eb53f6e
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-07-08 08:41:16 -0300

    Update 2plotPSMC.sh

--- Demography/2plotPSMC.sh
+++ Demography/2plotPSMC.sh
@@ -1,22 +1,22 @@
-#zebra finch mutation rate Naurim et al., 2010
+# : G Thom. et al, 2018. Taxa de mutação de Thamnophilidae; doi.org/10.1093/sysbio/syy004)
 # generation time according bird of life speluncae/novacapitalis: 3,2; diamentinesis/petrophilus: 3,1; pachecoi: 2,9; superciliaris: 3
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G BocainaSerradoMarRJ1_lin4.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G Caparao_lin3.d5.D50.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G CentroLestePR1_lin7.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G CentroOesteSC1_lin7.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G CunhaSerradoMarRJ2_lin4.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G DevonianaPR3_lin5.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G DevonianaPR8.d10.D90.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G ExtremoSulRS3_lin7.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G NorteMantiqueira3_lin3.d6.D60.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G NortePR3_lin5.d6.D60.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G NortePR_lin7.d6.D60.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.1 -p -R -G Sdiamantinensis.d7.D70.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G SerradaOuricana_lin1.d5.D45.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G SerradasLontra4_lin1.d6.D60.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G SerradosOrgaos3_lin2.d3.D30.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G Snovacapitalis.d10.D90.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 2.9 -p -R -G Spachecoi.d10.D90.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.1 -p -R -G Spetrophilus.d5.D50.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.0 -p -R -G Ssuperciliaris.d3.D30.diploid.psmc
-/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.00000000221 -g 3.2 -p -R -G SulMantiqueira3_lin3.d7.D66.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G BocainaSerradoMarRJ1_lin4.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G Caparao_lin3.d5.D50.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G CentroLestePR1_lin7.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G CentroOesteSC1_lin7.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G CunhaSerradoMarRJ2_lin4.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G DevonianaPR3_lin5.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G DevonianaPR8.d10.D90.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G ExtremoSulRS3_lin7.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G NorteMantiqueira3_lin3.d6.D60.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G NortePR3_lin5.d6.D60.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G NortePR_lin7.d6.D60.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.1 -p -R -G Sdiamantinensis.d7.D70.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G SerradaOuricana_lin1.d5.D45.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G SerradasLontra4_lin1.d6.D60.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G SerradosOrgaos3_lin2.d3.D30.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G Snovacapitalis.d10.D90.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 2.9 -p -R -G Spachecoi.d10.D90.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.1 -p -R -G Spetrophilus.d5.D50.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.0 -p -R -G Ssuperciliaris.d3.D30.diploid.psmc
+/media/labgenoma5/DATAPART1/bandriola/Softwares/psmc/utils/psmc_plot.pl -u 0.0000000025 -g 3.2 -p -R -G SulMantiqueira3_lin3.d7.D66.diploid.psmc
