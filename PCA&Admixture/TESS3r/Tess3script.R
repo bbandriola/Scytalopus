20590e5f1f3a45ab3727e63e01a0d87c563f4010
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-07-13 10:44:44 -0300

    Update Tess3script.R

--- PCA&Admixture/TESS3r/Tess3script.R
+++ PCA&Admixture/TESS3r/Tess3script.R
@@ -1,5 +1,27 @@
 # covert vcf to ped file with plink 
 # plink --vcf vcf --recode12 --out outputprefix
 
+library(devtools)
 library(tess3r)
-library(
+library(terra)
+library(raster)
+library(ggplot2)
+library(maps)
+
+setwd("~/Desktop/Projetos/Sspeluncae/Analises/TESS/")
+
+
+
+k6 <- read.table("k6.txt")
+coord <- read.table("coord.txt", sep="\t")
+coord_asmatrix <-as.matrix(coord)
+
+my.colors <- c("#4D9F8C", "#7570B3", "#E8488B", "#66A61D","#E93F33","#E6AC2F")
+my.palette <- CreatePalette(my.colors)
+plot(k6, coord_asmatrix, method = "map.max", interpol = FieldsKrigModel(10),  
+     main = "Ancestry coefficients",
+     xlab = "Longitude", ylab = "Latitude", 
+     resolution = c(300,300), cex = .4, 
+     col.palette = my.palette)
+
+map<-
