b42cf4e65fd438d0b3df9e70ff3a5b50ccf557b4
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-07-14 16:49:39 -0300

    Update Treemix.sh

--- IntrogressionTest/Treemix.sh
+++ IntrogressionTest/Treemix.sh
@@ -1,5 +1,7 @@
 # based on https://speciationgenomics.github.io/Treemix/ tutorial
 # vcf to be used: Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz
+# for run 2 use a vcf with novacapitalis as outgroup 
+# bcftools view -s ^Ssuperciliaris,Sdiamantinensis,Spachecoi,Spetrophilus -Oz -o Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05_Snovacapitalusasoutgroup.recode.vcf.gz Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz
 
 # 1. generate clst file from ADMIXTURE file
 bcftools query -l ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz | awk '{split($1,pop,"."); print $1"\t"$1"\t"pop[2]}' > sspeluncae.clust
@@ -10,6 +12,7 @@ vcf2treemix.sh ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_Filtered
 # 3. run treemix
 # specify how many migration edges it should add
 # run from 0 - 7, setting Ssuperciliaris as outgroup 
+# conda activate Treemix
 for i in {0..7}
 do
  treemix -i ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.treemix.frq.gz -m $i -o Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.$i -root Ssuperciliaris -bootstrap -k 500 -noss > treemix_${i}_log &
