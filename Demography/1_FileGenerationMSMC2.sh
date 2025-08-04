4e4ac943bf6dae8a870715bc4d771e41f5b06af9
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-04 17:12:49 -0300

    Update 1_FileGenerationMSMC2.sh

--- Demography/1_FileGenerationMSMC2.sh
+++ Demography/1_FileGenerationMSMC2.sh
@@ -1,2 +1,26 @@
-# following tutorial https://github.com/stschiff/msmc-tools/blob/master/msmc-tutorial/guide.md
-# 1. file generation
+#!/bin/bash
+# 1. file generation for every ind of the populations
+# will generate VCF for every scaffold for every ind and a mask file
+
+BAM_LIST="bam_list.txt"
+COV_LIST="coverage.txt"
+REF="ref.fa"
+NUM_SCAFFOLDS=10  # Change this to the actual number of scaffolds
+
+mapfile -t BAMS < "$BAM_LIST"
+mapfile -t COVS < "$COV_LIST"
+
+for i in "${!BAMS[@]}"; do
+    BAM="${BAMS[$i]}"
+    COV="${COVS[$i]}"
+    SAMPLE=$(basename "$BAM" .bam)
+
+    for SCAFFOLD in $(seq 1 $NUM_SCAFFOLDS); do
+        samtools mpileup -B -q 20 -Q 20 -C 50 -g -r "$SCAFFOLD" -f "$REF" "$BAM" | \
+        bcftools call -c -V indels | \
+        ./bamCaller.py "$COV" "${SAMPLE}_scaffold${SCAFFOLD}_mask.bed.gz" | \
+        gzip -c > "${SAMPLE}_scaffold${SCAFFOLD}_outMSMC2.vcf.gz"
+    done
+done
+
+# where you need to give the average sequencing depth as <mean_cov>
