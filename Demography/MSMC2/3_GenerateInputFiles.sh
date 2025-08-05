beccf10cb4c91523ce97b973344fcb92c1b20493
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-05 15:49:18 -0300

    Update 3_GenerateInputFiles.sh

--- Demography/MSMC2/3_GenerateInputFiles.sh
+++ Demography/MSMC2/3_GenerateInputFiles.sh
@@ -1,2 +1,7 @@
 # Combining Multiple Individuals into One Input File
 # script generate_multihetsep.py from MSMC-tools
+git clone https://github.com/stschiff/msmc-tools.git
+
+# generate msmc2 input from mask files and VCFs
+# generate a loop to every 
+msmc-tools.git/generate_multihetsep.py --mask maskfile.bed.gz --mask vcf.gz > ind_multihetsep.txt
