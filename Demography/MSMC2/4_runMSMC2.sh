2aa2b30be45bab76c289246029614668dee15407
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-05 16:51:02 -0300

    Update 4_runMSMC2.sh

--- Demography/MSMC2/4_runMSMC2.sh
+++ Demography/MSMC2/4_runMSMC2.sh
@@ -1,4 +1,10 @@
 # run msmc
-msmc2 --nrThreads 10 -I 0-1,2-3,4-5,6-7 -o outputprefix inputpop_*_multihetsep.txt
+msmc2 --nrThreads 10 -p "2+2+30*2+4+6+10" -o outputprefix $OUTDIR/pop1.chr*.multihetsep.txt
 
-# bootstrap 
+# 20 bootstrap
+msmc-tools/multihetsep_bootstrap.py -n 20 --nr_chromosomes 2 --seed 8 /bootstrap/pop1_bs $OUTDIR/pop1.chr*.multihetsep.txt
+$ for i in {1..20}; do 
+msmc2 --nrThreads 5 -o /bootstrap/pop1_bs$i /bootstrap/pop1_bs_$i/bootstrap*.txt; done
+
+# plot graphic 
+#wget 
