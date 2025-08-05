96c79323bc8b3faa35e409ab156e367ee92f0561
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-05 13:49:45 -0300

    Update 2_GenerateMappabilityMask_MSMC2.sh

--- Demography/2_GenerateMappabilityMask_MSMC2.sh
+++ Demography/2_GenerateMappabilityMask_MSMC2.sh
@@ -1,1 +1,7 @@
+# using SNPable pipeline
 
+wget https://github.com/grenaud/mappability_snpable/blob/main/snpable.bash
+git clone https://github.com/lh3/misc.git
+cd misc/seq/seqbility && make
+
+# usage: bash snpable.bash ref.fa /storage/software/lh3_misc/seq/seqbility/
