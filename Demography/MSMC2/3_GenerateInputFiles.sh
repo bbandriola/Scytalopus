460148424b2e589e3bdba762bad5ea588595db18
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-05 16:00:56 -0300

    Update 3_GenerateInputFiles.sh

--- Demography/MSMC2/3_GenerateInputFiles.sh
+++ Demography/MSMC2/3_GenerateInputFiles.sh
@@ -4,4 +4,17 @@ git clone https://github.com/stschiff/msmc-tools.git
 
 # generate msmc2 input from mask files and VCFs
 # generate a loop to every 
-msmc-tools.git/generate_multihetsep.py --mask maskfile.bed.gz --mask vcf.gz > ind_multihetsep.txt
+for maskfile in *_maskfile.bed.gz; do
+    # Extract the sampleid and scaffold number from the filename
+    base=$(basename "$maskfile"_maskfile.bed.gz)
+
+    # Construct the names of the other required files
+    vcfmask="${base}_outMSMC2vcf.gz"
+    output="${base}_multihetsep.txt"
+
+    # Run the command
+    msmc-tools.git/generate_multihetsep.py \
+        --mask "$maskfile" \
+        --mask "$vcfmask" \
+        > "$output"
+done
