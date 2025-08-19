21991cba793ccd0415fd2b78ca76648d4ce9ff6d
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-19 09:36:10 -0300

    Update Fst.sh

--- Diversity/Fst.sh
+++ Diversity/Fst.sh
@@ -1,25 +1,28 @@
 # Fst with vcftools 
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin2.txt --out fstlin1_lin2
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin3.txt --out fstlin1_lin3
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin4.txt --out fstlin1_lin4
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin5.txt --out fstlin1_lin5
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin6.txt --out fstlin1_lin6
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin1.txt --weir-fst-pop lin7.txt --out fstlin1_lin7
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin2.txt --weir-fst-pop lin3.txt --out fstlin2_lin3
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin2.txt --weir-fst-pop lin4.txt --out fstlin2_lin4
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin2.txt --weir-fst-pop lin5.txt --out fstlin2_lin5
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin2.txt --weir-fst-pop lin6.txt --out fstlin2_lin6
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin2.txt --weir-fst-pop lin7.txt --out fstlin2_lin7
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin3.txt --weir-fst-pop lin4.txt --out fstlin3_lin4
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin3.txt --weir-fst-pop lin5.txt --out fstlin3_lin5
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin3.txt --weir-fst-pop lin6.txt --out fstlin3_lin6
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin3.txt --weir-fst-pop lin7.txt --out fstlin3_lin7
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin4.txt --weir-fst-pop lin5.txt --out fstlin4_lin5
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin4.txt --weir-fst-pop lin6.txt --out fstlin4_lin6
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin4.txt --weir-fst-pop lin7.txt --out fstlin4_lin7
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin5.txt --weir-fst-pop lin6.txt --out fstlin5_lin6
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin5.txt --weir-fst-pop lin7.txt --out fstlin5_lin7 
-vcftools --gzvcf ../vcffiles/OnlySspeluncae_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz --fst-window-size 50000 --fst-window-step 10000 --weir-fst-pop lin6.txt --weir-fst-pop lin7.txt --out fstlin6_lin7
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin2.txt --out fstlin1_lin2
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin3.txt --out fstlin1_lin3
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin4.txt --out fstlin1_lin4
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin57.txt --out fstlin1_lin57
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin6.txt --out fstlin1_lin6
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin1.txt --weir-fst-pop lin7.txt --out fstlin1_lin7
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin2.txt --weir-fst-pop lin3.txt --out fstlin2_lin3
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin2.txt --weir-fst-pop lin4.txt --out fstlin2_lin4
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin2.txt --weir-fst-pop lin57.txt --out fstlin2_lin57
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin2.txt --weir-fst-pop lin6.txt --out fstlin2_lin6
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin2.txt --weir-fst-pop lin7.txt --out fstlin2_lin7
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin3.txt --weir-fst-pop lin4.txt --out fstlin3_lin4
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin3.txt --weir-fst-pop lin57.txt --out fstlin3_lin57
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin3.txt --weir-fst-pop lin6.txt --out fstlin3_lin6
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin3.txt --weir-fst-pop lin7.txt --out fstlin3_lin7
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin4.txt --weir-fst-pop lin57.txt --out fstlin4_lin57
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin4.txt --weir-fst-pop lin6.txt --out fstlin4_lin6
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin4.txt --weir-fst-pop lin7.txt --out fstlin4_lin7
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin57.txt --weir-fst-pop lin6.txt --out fstlin57_lin6
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin57.txt --weir-fst-pop lin7.txt --out fstlin57_lin7 
+vcftools --gzvcf ../../../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_FilteredPCA_GeographicNames_onlyspeluncae.vcf.gz.recode.vcf.gz --weir-fst-pop lin6.txt --weir-fst-pop lin7.txt --out fstlin6_lin7
+  # heatmap with the result 
+  Rscript heatmap.r
+
 # Fst with pixy according hudson method (ref:https://www.biorxiv.org/content/10.1101/2024.09.24.614506v1.full)
 pixy --stats fst --vcf ../../vcffiles/FinalVCFs/OnlySspeluncae_WithoutLin1_FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames.vcf.gz --populations AllSspeluncaePops.txt --window_size 100000 --n_cores 4 --output_prefix AllSspeluncaePops_WithoutLin1_FstHudson --fst_type hudson --bypass_invariant_check 'yes'
   # between populations according PCA
