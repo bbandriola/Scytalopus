d938906d221dfd47b248c0432c8ddc132f2493b2
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-26 21:14:53 -0300

    Update IdentifyChrWZ.sh

--- SexIdIndividuals/IdentifyChrWZ.sh
+++ SexIdIndividuals/IdentifyChrWZ.sh
@@ -1,1 +1,3 @@
-
+# with minimap2
+~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae103_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > NortePR5_lin7.WChr.sam
+~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae116_lin3_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > Caparao_lin3.WChr.sam
