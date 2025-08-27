d84e766bedcbb189728545ae8a6f0da689d684ff
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-08-27 09:56:00 -0300

    Update IdentifyChrWZ.sh

--- SexIdIndividuals/IdentifyChrWZ.sh
+++ SexIdIndividuals/IdentifyChrWZ.sh
@@ -1,7 +1,6 @@
 # with minimap2
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae103_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > NortePR5_lin7.WChr.sam
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae116_lin3_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > Caparao_lin3.WChr.sam
-
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae101_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > NortePR4_lin7.WChr.sam
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae106_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > CentroLestePR1_lin7.WChr.sam
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae107_lin5_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > DevonianaPR5_lin5.WChr.sam
@@ -54,3 +53,67 @@
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae87_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > NortePR3_lin7.WChr.sam
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae95_lin7_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > NorteMantiqueira1_lin3.WChr.sam
 ~/Softwares/minimap2-2.30_x64-linux/minimap2 -a -t 5 /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/bams/Fastas/masked_fastas/Sspeluncae9_lin3_final.bam.fa.bam_masked.fa Wchr_Clanceolata.fasta > ExtremoSulRS3_lin7.WChr.sam
+
+# convert sam to fasta 
+samtools fasta BoaEsperanca1_lin6.WChr.sam > BoaEsperanca1_lin6.WChr.fasta
+samtools fasta BoaEsperanca2_lin6.WChr.sam > BoaEsperanca2_lin6.WChr.fasta
+samtools fasta BoaEsperanca3_lin7.WChr.sam >.WChr.sam > BoaEsperanca3_lin7.WChr.sam >.WChr.fasta
+samtools fasta BocainaSerraDoMarRJ1_lin4.WChr.sam > BocainaSerraDoMarRJ1_lin4.WChr.fasta
+samtools fasta BocainaSerraDoMarRJ3_lin4.WChr.sam > BocainaSerraDoMarRJ3_lin4.WChr.fasta
+samtools fasta BocainaSerraDoMarRJ4_lin4.WChr.sam > BocainaSerraDoMarRJ4_lin4.WChr.fasta
+samtools fasta Caparao_lin3.WChr.sam > Caparao_lin3.WChr.fasta
+samtools fasta CentroLestePR1_lin7.WChr.sam > CentroLestePR1_lin7.WChr.fasta
+samtools fasta CentroLesteSC1_lin7.WChr.sam >.WChr.sam > CentroLesteSC1_lin7.WChr.sam >.WChr.fasta
+samtools fasta CentroOesteSC1_lin7.WChr.sam > CentroOesteSC1_lin7.WChr.fasta
+samtools fasta CentroOesteSC2_lin7.WChr.sam > CentroOesteSC2_lin7.WChr.fasta
+samtools fasta CentroOesteSC3_lin7.WChr.sam > CentroOesteSC3_lin7.WChr.fasta
+samtools fasta CentroOesteSC4_lin7.WChr.sam > CentroOesteSC4_lin7.WChr.fasta
+samtools fasta CentroOesteSC5_lin7.WChr.sam > CentroOesteSC5_lin7.WChr.fasta
+samtools fasta CunhaSerraDoMarRJ2_lin4.WChr.sam > CunhaSerraDoMarRJ2_lin4.WChr.fasta
+samtools fasta DevonianaPR2_lin5.WChr.sam > DevonianaPR2_lin5.WChr.fasta
+samtools fasta DevonianaPR3_lin5.WChr.sam > DevonianaPR3_lin5.WChr.fasta
+samtools fasta DevonianaPR4_lin5.WChr.sam > DevonianaPR4_lin5.WChr.fasta
+samtools fasta DevonianaPR5_lin5.WChr.sam > DevonianaPR5_lin5.WChr.fasta
+samtools fasta DevonianaPR6_lin5.WChr.sam > DevonianaPR6_lin5.WChr.fasta
+samtools fasta DevonianaPR7_lin5.WChr.sam > DevonianaPR7_lin5.WChr.fasta
+samtools fasta DevonianaPR8_lin5.WChr.sam > DevonianaPR8_lin5.WChr.fasta
+samtools fasta DevonianaSP1_lin5.WChr.sam > DevonianaSP1_lin5.WChr.fasta
+samtools fasta DevonianaSP2_lin5.WChr.sam > DevonianaSP2_lin5.WChr.fasta
+samtools fasta DevonianaSP3_lin5.WChr.sam > DevonianaSP3_lin5.WChr.fasta
+samtools fasta DevonianaSP4_lin5.WChr.sam > DevonianaSP4_lin5.WChr.fasta
+samtools fasta ExtremoSulRS2_lin7.WChr.sam > ExtremoSulRS2_lin7.WChr.fasta
+samtools fasta ExtremoSulRS3_lin7.WChr.sam > ExtremoSulRS3_lin7.WChr.fasta
+samtools fasta ExtremoSulSC1_lin7.WChr.sam > ExtremoSulSC1_lin7.WChr.fasta
+samtools fasta HCentroLesteSC_lin7.WChr.sam > HCentroLesteSC_lin7.WChr.fasta
+samtools fasta HNortePR_lin7.WChr.sam > HNortePR_lin7.WChr.fasta
+samtools fasta NorteMantiqueira1_lin3.WChr.sam > NorteMantiqueira1_lin3.WChr.fasta
+samtools fasta NorteMantiqueira2_lin3.WChr.sam > NorteMantiqueira2_lin3.WChr.fasta
+samtools fasta NorteMantiqueira3_lin3.WChr.sam > NorteMantiqueira3_lin3.WChr.fasta
+samtools fasta NorteMantiqueira4_lin3.WChr.sam > NorteMantiqueira4_lin3.WChr.fasta
+samtools fasta NortePR1_lin7.WChr.sam > NortePR1_lin7.WChr.fasta
+samtools fasta NortePR2_lin7.WChr.sam > NortePR2_lin7.WChr.fasta
+samtools fasta NortePR3_lin7.WChr.sam > NortePR3_lin7.WChr.fasta
+samtools fasta NortePR4_lin7.WChr.sam > NortePR4_lin7.WChr.fasta
+samtools fasta NortePR5_lin7.WChr.sam > NortePR5_lin7.WChr.fasta
+samtools fasta SerraDaOuricana1_lin1.WChr.sam > SerraDaOuricana1_lin1.WChr.fasta
+samtools fasta SerraDaOuricana2_lin1.WChr.sam > SerraDaOuricana2_lin1.WChr.fasta
+samtools fasta SerraDaOuricana3_lin1.WChr.sam > SerraDaOuricana3_lin1.WChr.fasta
+samtools fasta SerraDaOuricana4_lin1.WChr.sam > SerraDaOuricana4_lin1.WChr.fasta
+samtools fasta SerraDosOrgaos3_lin2.WChr.sam > SerraDosOrgaos3_lin2.WChr.fasta
+samtools fasta SerradaLontras2_lin1.WChr.sam > SerradaLontras2_lin1.WChr.fasta
+samtools fasta SerradaLontras3_lin1.WChr.sam > SerradaLontras3_lin1.WChr.fasta
+samtools fasta SerradaLontras4_lin1.WChr.sam > SerradaLontras4_lin1.WChr.fasta
+samtools fasta SerradaLontras5_lin1.WChr.sam > SerradaLontras5_lin1.WChr.fasta
+samtools fasta Spetrophilus.WChr.sam > Spetrophilus.WChr.fasta
+samtools fasta SulMantiqueira2_lin3.WChr.sam > SulMantiqueira2_lin3.WChr.fasta
+samtools fasta SulMantiqueira3_lin3.WChr.sam > SulMantiqueira3_lin3.WChr.fasta
+samtools fasta SulMantiqueira4_lin3.WChr.sam > SulMantiqueira4_lin3.WChr.fasta
+samtools fasta SulMantiqueira5_lin3.WChr.sam > SulMantiqueira5_lin3.WChr.fasta
+samtools fasta SerradaLontras3_lin1.WChr.sam > SerradaLontras3_lin1.WChr.fasta
+samtools fasta SerradaLontras4_lin1.WChr.sam > SerradaLontras4_lin1.WChr.fasta
+samtools fasta SerradaLontras5_lin1.WChr.sam > SerradaLontras5_lin1.WChr.fasta
+samtools fasta Spetrophilus.WChr.sam > Spetrophilus.WChr.fasta
+samtools fasta SulMantiqueira2_lin3.WChr.sam > SulMantiqueira2_lin3.WChr.fasta
+samtools fasta SulMantiqueira3_lin3.WChr.sam > SulMantiqueira3_lin3.WChr.fasta
+samtools fasta SulMantiqueira4_lin3.WChr.sam > SulMantiqueira4_lin3.WChr.fasta
+samtools fasta SulMantiqueira5_lin3.WChr.sam > SulMantiqueira5_lin3.WChr.fasta
