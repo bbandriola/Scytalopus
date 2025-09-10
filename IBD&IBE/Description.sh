9eb612750b7e912e68d52a796f36edd33c967cbf
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-09-10 13:36:45 -0300

    Update and rename Description to Description.sh

---
+++ IBD&IBE/Description.sh
@@ -0,0 +1,2 @@
+# convert VCF 2 DIS https://github.com/BGI-shenzhen/VCF2Dis
+VCF2DIS -Input ../vcffiles/FinalVCFs/OnlySspeluncae_FilteredPCA_FilteredMax10missingDepthmin10max100_GeographicNames_allsamples.vcf.gz -Out Sspeluncae_FilteredPCA_FilteredMax10missingDepthmin10max100_GeographicNames
