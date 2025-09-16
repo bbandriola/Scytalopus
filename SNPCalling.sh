e397bc3d4cdc3f8a4c8aba9832cd9564c4f840d1
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-09-16 16:18:10 -0300

    Update SNPCalling.sh

--- SNPCalling.sh
+++ SNPCalling.sh
@@ -3,21 +3,21 @@
 #BEFORE EVERYTHING: 
 # normalizar os vcfs antes de fazer o merge
 # command:
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta121-127_raw_norm.vcf.gz Scyta121-127_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta139-154_raw_norm.vcf.gz Scyta139-154_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta1_raw_norm.vcf.gz Scyta1_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta95-110_raw_norm.vcf.gz Scyta95-110_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_1-15_48-64_raw_norm.vcf.gz Scyta_1-15_48-64_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_116-120_raw_norm.vcf.gz Scyta_116-120_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_16-28_46-47_raw_norm.vcf.gz Scyta_16-28_46-47_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_43_68-74_raw_norm.vcf.gz Scyta_43_68-74_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_75-87_113-114_raw_norm.vcf.gz Scyta_75-87_113-114_raw.vcf.gz
-bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_ira_psy_indi_super_raw_norm.vcf.gz Scyta_ira_psy_indi_super_raw.vcf.gz
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta121-127_raw_norm.vcf.gz Scyta121-127_raw.vcf.gz # snps 
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta139-154_raw_norm.vcf.gz Scyta139-154_raw.vcf.gz # snps 
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta1_raw_norm.vcf.gz Scyta1_raw.vcf.gz # snps 
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta95-110_raw_norm.vcf.gz Scyta95-110_raw.vcf.gz # snps 
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_1-15_48-64_raw_norm.vcf.gz Scyta_1-15_48-64_raw.vcf.gz # snps 152678617
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_116-120_raw_norm.vcf.gz Scyta_116-120_raw.vcf.gz # snps 38593591
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_16-28_46-47_raw_norm.vcf.gz Scyta_16-28_46-47_raw.vcf.gz # snps 46539656
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_43_68-74_raw_norm.vcf.gz Scyta_43_68-74_raw.vcf.gz # snps 42889405
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_75-87_113-114_raw_norm.vcf.gz Scyta_75-87_113-114_raw.vcf.gz # snps 47742392
+bcftools norm -f ../../../../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa -Oz -o Scyta_ira_psy_indi_super_raw_norm.vcf.gz Scyta_ira_psy_indi_super_raw.vcf.gz # snps 5708314 
 
 
 # 1. merge all generated vcf from snpArcher project's into a single vcf
 # https://samtools.github.io/bcftools/bcftools.html#merge
-bcftools merge -m snps --output mergedVCFproject0_1_2_3_4_5_6_7_8_9.vcf.gz --output-type z --threads 7 --file-list ../vcffiles2merge.txt
+bcftools merge -m snps --output mergeallvcfsfromsnparcher.vcf.gz -Oz --threads 7 --file-list vcffiles2merge.txt #rodando
 # results: 68 individuals | 206.000.140 SNPs
   # 1.1 change for geographic names 
   bcftools reheader --samples GeographicNames.txt --output GeographicNames_allsamples.vcf.gz mergedVCFproject0_1_2_3_4_5_6_7_8_9.vcf.gz
