5e7b3f144ea4a6a0038f4d749fd756f4513f4ea2
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-09-21 10:50:09 -0300

    Update IBD_IBE.R

--- IBD&IBE/IBD_IBE.R
+++ IBD&IBE/IBD_IBE.R
@@ -54,18 +54,61 @@ row.names(gene_matrix) <- gene_matrix$NA.
 gene_matrix[1] <- NULL
 
 
-#carregar variaveis de dis geo e gene para cada par de populações
+#### RUN MANTEL TEST GEOGRAPHIC AND GENETIC DISTANCE#####
+# run in linux to convert VCF into a distance matrix with VCF2DIS script 
+# Carrega matriz genética gerada com o ngsdist (beagle) ou VCF2Dis (VCF)
+# VCF2Dis calcula p-distance = numero de diferenças nucleotidicas / numero de nucleotideos comparados
+# command: VCF2DIS -Input ../vcffiles/FinalVCFs/OnlySspeluncae_FilteredPCA_FilteredMax10missingDepthmin10max100_GeographicNames_allsamples.vcf.gz -Out Sspeluncae_FilteredPCA_FilteredMax10missingDepthmin10max100_GeographicNames
+
+# As matrizes precisam ser iguais em tamanho (mesmo numero de ind) e ter a mesma ordem
+# Matriz geografica é gerada a partir de coordenadas no formato UTM
+# data <- read.table("56samples_UTM_coordinates.csv", sep = ",", header = T)
+# ge <- data[,4:5] #pega só as colunas latitude e longitude da tabela
+# test <- dist(ge, method = "euclidean", diag = TRUE, upper = TRUE) #gera matriz dist euclidiana
+# write.table(as.matrix(test), "utm_matrix_56samples_Ponca.txt") #salva em um arquivo
+UTM <- read.table("UTM.csv",sep="",header=T)
+longlat <- data.frame(UTM$Longitude,UTM$Latitude)
+testUTM <- dist(longlat, method = "euclidean", diag = TRUE, upper = TRUE)
+write.table(as.matrix(testUTM), "utm_matrixeuclideanist.txt")
+
+# after convert both dataset in distance, load both variables to each pair population that will be tested
 # Quando ja tiver uma matriz, o comando abaixo serve para carrega-la
-geo_matrix <- read.xlsx("utm_lin1vslin2.xlsx", 1, header=F)
-row.names(geo_matrix) <- geo_matrix$X1
+#geo_matrix <- read.xlsx("utm_lin1vslin3.xlsx", 1, header=T)
+#row.names(geo_matrix) <- geo_matrix$NA.
+#geo_matrix[1] <- NULL
 #geo_matrix <- geo_matrix[-c(48),]
-gene_matrix <- read.xlsx("GenDist_lin1vslin2.xlsx",1, header = TRUE)
-row.names(gene_matrix) <- gene_matrix$X1
-
-
-
-
+#gene_matrix <- read.xlsx("genedist_lin1vslin2.xlsx",1, header = TRUE)
+#row.names(gene_matrix) <- gene_matrix$NA.
+#gene_matrix[1] <- NULL
+# Convert to dist objects
+#gene_dist <- as.dist(gene_matrix)
+#geo_dist  <- as.dist(geo_matrix)
 #Teste de Mantel
-results_mantel <- mantel(as.dist(gene_matrix) ~ as.dist(geo_matrix), nperm = 10000) # as.dist muda a classe do objeto
+#mantel(gene_dist, geo_dist, method = "pearson", permutations = 10000) # as.dist muda a classe do objeto
 # results_mantel <- mantel(as.dist(geo_matrix) ~ as.dist(gene_matrix)) #testar ao contrario
-results_mantel
+#results_mantel
+
+run_mantel <- function(geo_file, gene_file, sheet_geo = 1, sheet_gene = 1, permutations = 10000) {
+  # Read geographic matrix
+  geo_matrix <- read.xlsx(geo_file, sheet_geo, header = TRUE)
+  row.names(geo_matrix) <- geo_matrix[,1]   # use first column as row names
+  geo_matrix <- geo_matrix[,-1]             # remove first column
+  
+  # Read genetic matrix
+  gene_matrix <- read.xlsx(gene_file, sheet_gene, header = TRUE)
+  row.names(gene_matrix) <- gene_matrix[,1] # use first column as row names
+  gene_matrix <- gene_matrix[,-1]           # remove first column
+  
+  # Convert to distance objects
+  geo_dist  <- as.dist(geo_matrix)
+  gene_dist <- as.dist(gene_matrix)
+  
+  # Run Mantel test
+  results_mantel <- mantel(gene_dist, geo_dist, method = "pearson", permutations = permutations)
+  
+  return(results_mantel)
+}
+
+geo_file="utm_lin1vslin4.xlsx"
+gene_file="genedist_lin1vslin4.xlsx"
+run_mantel(geo_file,gene_file)
