db63ae04bb8f8d95e498155dccb14dbd56f28db8
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-09-25 16:18:34 -0300

    Update AdmixtureplotONLYSpseluncae.r

--- PCA&Admixture/AdmixtureplotONLYSpseluncae.r
+++ PCA&Admixture/AdmixtureplotONLYSpseluncae.r
@@ -1,5 +1,24 @@
-# North lineages colors: c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C"))
-# South lineages: "#EEAEEE","#E31A1C","#FDBF6F"
+# correct color orders
+pdf("K5_SspeluncaeNoZW.pdf", height = 5, width = 14)
+tbl=read.table("NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.5.Q")
+tbl$V1 <- factor(tbl$V1, levels = unique(tbl$V1))
+tbl_long <- pivot_longer(tbl, cols = V2:V6, names_to = "K", values_to = "Proportion")
+ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
+  geom_col(width = 1, color = NA) +  # Full-width bars
+  scale_fill_manual(
+    values = c("V2" = "#7570B3", "V3" = "#E93F33", "V4" = "#E6AC2F",
+               "V5" = "#66A61D", "V6" = "deeppink"),
+    labels = c("K=1","K=2","K=3","K=4","K=5")
+  ) +
+  labs(x = "Individuals", y = "Ancestry Proportion") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+    panel.grid = element_blank()
+  )
+
+dev.off()
+########
 
 pdf("K1_ONLYSspeluncae.pdf", height = 5, width = 6)
 tbl=read.table("SpeluncaeONLY_Filtered_mergedVCFproject1_2_5_6_7_8.1.Q")
