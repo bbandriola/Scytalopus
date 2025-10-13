dffbea2a65682c52a1ddd591784371e4da0eae82
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-13 14:58:56 -0300

    Update run.sh

--- IntrogressionTest/HyDe/run.sh
+++ IntrogressionTest/HyDe/run.sh
@@ -8,4 +8,4 @@ python run_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_Geograp
 # 3. testing for the dynamic of the introgression between individuals (to understand if is uniform or not)
 # it needs the input of the triplets. So we would test only the populations that seemed introgressed 
 # the triple file can be the output of the run_hyde.py run
-python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1Snovacap.phy -m map.txt -tr triplets2teste.txt -o out -n 55 -t 8 -s 1801056
+python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1Snovacap.phy -m map.txt -tr triplets2test.txt -o out -n 55 -t 8 -s 1801056
