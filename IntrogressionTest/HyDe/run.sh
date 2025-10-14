c9bff6216d2e3a149dc49d9018dca3b0814d4369
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-14 09:06:53 -0300

    Update run.sh

--- IntrogressionTest/HyDe/run.sh
+++ IntrogressionTest/HyDe/run.sh
@@ -9,5 +9,5 @@ python run_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_Geograp
 # it needs the input of the triplets. So we would test only the populations that seemed introgressed 
 # the triple file can be the output of the run_hyde.py run
 python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1Snovacap.phy -m map.txt -tr triplets2test.txt -o out -n 55 -t 8 -s 1801056
-python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.phy -m map.txt -tr triplets2test.txt -o out -n 54 -t 7 -s 1636679 -prefix Individualtest_SpeluncaeLin1 
+python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.phy -m map.txt -tr triplets2test.txt -o Lin1 -n 54 -t 7 -s 1636679 --prefix Individualtest_SpeluncaeLin1 
 
