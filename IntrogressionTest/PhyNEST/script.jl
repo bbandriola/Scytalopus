d7af48c877b6e7437545bed9243b25ce46c5d937
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-15 10:43:00 -0300

    Update script.jl

--- IntrogressionTest/PhyNEST/script.jl
+++ IntrogressionTest/PhyNEST/script.jl
@@ -1,5 +1,18 @@
 # based on https://github.com/sungsik-kong/PhyNEST.jl/wiki/ tutorial 
 # "parse the input sequence alignment and compute observed quartet site pattern frequencies"
 phylip_data = readPhylip("filename.phy",checkpoint=true,writecsv=true)
-# check the site pattern matrix 
+  # check the site pattern matrix 
 df=show_sp(phylip_data)
+  # parse topology function
+starting_topology=readTopology("(Outgroup,(Lin1,((Lin2,Lin3),(Lin4,((Lin6,Lin7),Lin5)))));")
+  # compute the composite likelihood with the informed tree and phylip file 
+stats,topology_1=do_optimization(starting_topology,phylip_data)
+
+# RUN phynest
+  # 1. parallelization
+  using Distributed
+  length(Sys.cpu_info()) # check cores available for julia to work with 
+  addprocs(8) # choose the number of cores to be used 
+  @everywhere using PhyNEST # following this command run the phyne! function 
+# hmax = maximum number of hybridization 
+test1 = phyne!(starting_topology,phylip_data,"outgroup",do_hill_climbing=false,number_of_burn_in=30,hmax=5)
