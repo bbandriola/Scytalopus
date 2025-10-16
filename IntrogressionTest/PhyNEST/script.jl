5a24771766f10b364367bc8cf6b0d1eed4dad5ce
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-16 08:04:37 -0300

    Update script.jl

--- IntrogressionTest/PhyNEST/script.jl
+++ IntrogressionTest/PhyNEST/script.jl
@@ -1,10 +1,10 @@
 # based on https://github.com/sungsik-kong/PhyNEST.jl/wiki/ tutorial 
 # "parse the input sequence alignment and compute observed quartet site pattern frequencies"
-phylip_data = readPhylip("filename.phy",checkpoint=true,writecsv=true)
+phylip_data = readPhylip("combined1kb.phy",checkpoint=true,writecsv=true) 
   # check the site pattern matrix 
 df=show_sp(phylip_data)
   # parse topology function
-starting_topology=readTopology("(Outgroup,(Lin1,((Lin2,Lin3),(Lin4,((Lin6,Lin7),Lin5)))));")
+starting_topology=readTopology("(Snovacapitalis,(Lin1,((Lin2,Lin3),(Lin4,((Lin6,Lin7),Lin5)))));")
   # compute the composite likelihood with the informed tree and phylip file 
 stats,topology_1=do_optimization(starting_topology,phylip_data)
 
@@ -15,4 +15,5 @@ stats,topology_1=do_optimization(starting_topology,phylip_data)
   addprocs(8) # choose the number of cores to be used 
   @everywhere using PhyNEST # following this command run the phyne! function 
 # hmax = maximum number of hybridization 
-test1 = phyne!(starting_topology,phylip_data,"outgroup",do_hill_climbing=false,number_of_burn_in=30,hmax=5)
+test1 = phyne!(starting_topology,phylip_data,"Sdiamantinensis127win1kb",do_hill_climbing=false,number_of_burn_in=30,hmax=5)
+
