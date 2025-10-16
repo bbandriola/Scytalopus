2842f45171d13ab0673db152adb2b313a357d2f1
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-16 09:45:24 -0300

    Update WholeGenomeSNPsTrees.sh for new input files

--- Phylogenies/WholeGenomeSNPsTrees.sh
+++ Phylogenies/WholeGenomeSNPsTrees.sh
@@ -2,13 +2,16 @@
 
 #1. vcf to .phy
 # https://github.com/edgardomortiz/vcf2phylip
-python vcf2phylip.py --input ../vcffiles/Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.vcf.gz --output-prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8
+python vcf2phylip.py --input file.vcf.gz --output-prefix file
 
 # IQ-TREE: 
-iqtree -s Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --alrt 1000 -B 1000 -b 100 -o Snovacapitalis152_lin --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.100boots_UFb_ALR -T 5
+iqtree -s file.phy --alrt 1000 -B 1000 -b 100 -o Snovacapitalis152_lin --prefix fileprefix_IQtree -T 5
 
 # RAxML-NG:
-raxml-ng-mpi --msa Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --all --prefix Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8 --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
+raxml-ng-mpi --msa file.phy --all --prefix fileprefix_raxml --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
+
+# CASTER:
+../../Softwares/ASTER/bin/caster-site --output file.caster.newick --thread 5 --support 1 --input file.phy 2> allsamples.caster.log
 
 # NJ with VCF2Dis
 # https://github.com/hewm2008/VCF2Dis
@@ -17,9 +20,3 @@ raxml-ng-mpi --msa Filteredmissing0.75_mergedVCFproject1_2_5_6_7_8.min4.phy --al
 ../../Softwares/VCF2Dis-1.54/bin/VCF2Dis -i ../vcffiles/FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz -o allspecies_project1_2_5_6_7_8 -TreeMethod 1
   # 2 run
 ../../Softwares/VCF2Dis-1.54/bin/VCF2Dis -i ../vcffiles/FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.vcf.gz -o Second2_allspecies_project1_2_5_6_7_8_DistanceMatrix -TreeMethod 1 -OutPut p_dist.matrix
-
-# CASTER:
-# make phy file
-../Softwares/vcf2phy -i ../vcffiles/FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_5_6_7_8.vcf.gz --output-prefix FilteredMax30missingDepthmin5_mergedVCFproject0_1_2_5_6_7_8
-# generate tree
-../../Softwares/ASTER/bin/caster-site --output allsamples.caster.newick --thread 5 --support 1 --input allsamples.min4.phy 2> allsamples.caster.log
