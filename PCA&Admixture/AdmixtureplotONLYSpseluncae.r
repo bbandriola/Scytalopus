f3e904919d2b81385d7aec39c26e8058291f132b
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-27 20:33:52 -0300

    Refactor plotting to use ggsave for SVG files
    Removed unnecessary pdf() calls and added ggsave() for SVG output.

--- PCA&Admixture/AdmixtureplotONLYSpseluncae.r
+++ PCA&Admixture/AdmixtureplotONLYSpseluncae.r
@@ -1,8 +1,7 @@
-pdf("K3_Sspeluncae.pdf", height = 5, width = 14)
 tbl=read.table("FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.3.Q")
 tbl$V1 <- factor(tbl$V1, levels = unique(tbl$V1))
 tbl_long <- pivot_longer(tbl, cols = V2:V4, names_to = "K", values_to = "Proportion")
-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
+p<-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
   geom_col(width = 1, color = NA) +  # Full-width bars
   scale_fill_manual(
     values = c("V2" = "deeppink",
@@ -15,14 +14,12 @@ ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
     axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
     panel.grid = element_blank()
   )
+ggsave("K3_Sspeluncae.svg", plot = p, width = 14, height = 5)
 
-dev.off()
-
-pdf("K4_SspeluncaeNoZW.pdf", height = 5, width = 14)
 tbl=read.table("FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.4.Q")
 tbl$V1 <- factor(tbl$V1, levels = unique(tbl$V1))
 tbl_long <- pivot_longer(tbl, cols = V2:V5, names_to = "K", values_to = "Proportion")
-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
+p<-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
   geom_col(width = 1, color = NA) +  # Full-width bars
   scale_fill_manual(
     values = c("V2" = "#66A61D", "V3" = "#E6AC2F",
@@ -35,14 +32,12 @@ ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
     axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
     panel.grid = element_blank()
   )
+ggsave("K4_Sspeluncae.svg", plot = p, width = 14, height = 5)
 
-dev.off()
-
-pdf("K5_SspeluncaeNoZW.pdf", height = 5, width = 14)
 tbl=read.table("FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.5.Q")
 tbl$V1 <- factor(tbl$V1, levels = unique(tbl$V1))
 tbl_long <- pivot_longer(tbl, cols = V2:V6, names_to = "K", values_to = "Proportion")
-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
+p<-ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
   geom_col(width = 1, color = NA) +  # Full-width bars
   scale_fill_manual(
     values = c("V2" = "#7570B3", "V3" = "#E93F33", "V4" = "#E6AC2F",
@@ -55,7 +50,7 @@ ggplot(tbl_long, aes(x = V1, y = Proportion, fill = K)) +
     axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
     panel.grid = element_blank()
   )
-dev.off()
+ggsave("K5_Sspeluncae.svg", plot = p, width = 14, height = 5)
 
 pdf("K6_Sspeluncae.pdf", height = 5, width = 14)
 tbl=read.table("FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.6.Q")
