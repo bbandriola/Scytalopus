f3310075f23b954f3feaf4f66d8068c216502718
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-10-30 12:16:28 -0300

    Update script.jl

--- IntrogressionTest/PhyNEST/script.jl
+++ IntrogressionTest/PhyNEST/script.jl
@@ -5,11 +5,11 @@
 # ACTUAL RUN PhyNEST
 # based on https://github.com/sungsik-kong/PhyNEST.jl/wiki/ tutorial 
 # "parse the input sequence alignment and compute observed quartet site pattern frequencies"
-phylip_data = readPhylip("all1kbfilesSelectedInd.phy",checkpoint=true,writecsv=true) 
+phylip_data = readPhylip("FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0.15.min4.phy",checkpoint=true,writecsv=true,csvname="inputPhyNEST") 
   # check the site pattern matrix 
 df=show_sp(phylip_data)
   # parse topology function
-starting_topology=readTopology("(Snovacapitalis152win1kb,(Sspeluncae120,((Sspeluncae83,(Sspeluncae116,(Sspeluncae73,Sspeluncae113))),((Sspeluncae16,Sspeluncae2),(((Sspeluncae123,Sspeluncae54),((Sspeluncae19,Sspeluncae72),Sspeluncae52)),(Sspeluncae87,(Sspeluncae64,(Sspeluncae140,Sspeluncae59))))))));")
+starting_topology=readTopology("(Snovacapitalis,(SerradaLontras4_lin1,((SerraDosOrgaos3_lin2,(Caparao_lin3,(SulMantiqueira4_lin3,NorteMantiqueira3_lin3))),((CunhaSerraDoMarRJ2_lin4,BocainaSerraDoMarRJ1_lin4),(((BoaEsperanca3_lin7,BoaEsperanca1_lin6),((ExtremoSulSC1_lin7,CentroOesteSC4_lin7),HCentroLesteSC_lin7)),(NortePR3_lin7,(HNortePR_lin7,(DevonianaPR8_lin5,DevonianaPR2_lin5))))))));")
   # compute the composite likelihood with the informed tree and phylip file 
 stats,topology_1=do_optimization(starting_topology,phylip_data)
 
@@ -21,4 +21,8 @@ stats,topology_1=do_optimization(starting_topology,phylip_data)
   @everywhere using PhyNEST # following this command run the phyne! function 
 # hmax = maximum number of hybridization 
 test1 = phyne!(starting_topology,phylip_data,"Sdiamantinensis127win1kb",do_hill_climbing=false,number_of_burn_in=30,hmax=5)
-
+# "Conducts Patterson's D-statistic test. The result prints site pattern frequencies ABAB and ABBA used to compute the D-statistic, Z-score, and the p-value for each quartet tested"
+Dstat(p::Phylip, outgroup::String;
+        alpha=0.05, 
+        displayall=false,
+        writecsv=true,filename="")
