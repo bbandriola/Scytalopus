7219643ef2d8257148f07426ff895e9a9af2414c
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-03 13:19:01 -0300

    Revise starting topology and test parameters in script
    Updated starting topology for phylogenetic analysis and modified test parameters.

--- IntrogressionTest/PhyNEST/script.jl
+++ IntrogressionTest/PhyNEST/script.jl
@@ -9,9 +9,12 @@ phylip_data = readPhylip("FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD
   # check the site pattern matrix 
 df=show_sp(phylip_data)
   # parse topology function
-starting_topology=readTopology("(Snovacapitalis,(SerradaLontras4_lin1,((SerraDosOrgaos3_lin2,(Caparao_lin3,(SulMantiqueira4_lin3,NorteMantiqueira3_lin3))),((CunhaSerraDoMarRJ2_lin4,BocainaSerraDoMarRJ1_lin4),(((BoaEsperanca3_lin7,BoaEsperanca1_lin6),((ExtremoSulSC1_lin7,CentroOesteSC4_lin7),HCentroLesteSC_lin7)),(NortePR3_lin7,(HNortePR_lin7,(DevonianaPR8_lin5,DevonianaPR2_lin5))))))));")
+starting_topology=readTopology("(Snovacapitalis,((SerradaLontras4_lin1,SerradaLontras3_lin1),((SerraDosOrgaos3_lin2,(Caparao_lin3,(SulMantiqueira4_lin3,NorteMantiqueira3_lin3))),((CunhaSerraDoMarRJ2_lin4,BocainaSerraDoMarRJ1_lin4),(((BoaEsperanca3_lin7,BoaEsperanca1_lin6),((ExtremoSulSC1_lin7,CentroOesteSC4_lin7),HCentroLesteSC_lin7)),(NortePR3_lin7,(HNortePR_lin7,(DevonianaPR8_lin5,DevonianaPR2_lin5))))))));")
   # compute the composite likelihood with the informed tree and phylip file 
 stats,topology_1=do_optimization(starting_topology,phylip_data)
+
+### in case happens an error: readCheckPoint(FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0.15.min4.phy.ckp)
+
 
 # RUN phynest
   # 1. parallelization
@@ -20,7 +23,7 @@ stats,topology_1=do_optimization(starting_topology,phylip_data)
   addprocs(8) # choose the number of cores to be used 
   @everywhere using PhyNEST # following this command run the phyne! function 
 # hmax = maximum number of hybridization 
-test1 = phyne!(starting_topology,phylip_data,"Sdiamantinensis127win1kb",do_hill_climbing=false,number_of_burn_in=30,hmax=5)
+test1 = phyne!(starting_topology,phylip_data,"Snovacapitalis",do_hill_climbing=false,number_of_burn_in=30,hmax=5,filename="PhyNESR_sa_FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0")
 # "Conducts Patterson's D-statistic test. The result prints site pattern frequencies ABAB and ABBA used to compute the D-statistic, Z-score, and the p-value for each quartet tested"
 Dstat(p::Phylip, outgroup::String;
         alpha=0.05, 
