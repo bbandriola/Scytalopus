e665c49a58c2aad59e41fcb9085640b7fd7a9ac3
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-10 14:21:11 -0300

    Modify NeEstimator script for new input files
    Updated file references and added NeEstimator execution step.

--- Demography/NeEstimator.sh
+++ Demography/NeEstimator.sh
@@ -1,9 +1,14 @@
-# independent sites: OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1.vcf.gz
+# independent sites: OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15_GeographicNames_OnlySpeluncae.recode.vcf.gz from each lineage
 # 1. vcf to geno file using LEA R
 # plink
-plink --vcf OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1.vcf.gz --recode A --out GENOinput_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1
+plink --vcf prefix.vcf.gz --recode A --allow-extra-chr --out GENOinput_prefix
 # R
 library(LEA)
 # Read .raw, drop first columns, and write to .geno
 geno <- read.table("GENOinput_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1.raw", header = TRUE)
 write.geno(geno[, -(1:6)], "OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1.geno")
+
+# 2. NeEstimator
+./Ne2x
+Input file > GENOinput_*
+
