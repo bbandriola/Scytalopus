693a6687e3c0c4df93ead3fb8e5adbf4a7834d9b
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-13 09:38:43 -0300

    Update GONE.sh

--- Demography/GONE.sh
+++ Demography/GONE.sh
@@ -1,1 +1,5 @@
 # GONE 
+# 1. generate file ped
+vcftools --gzvcf Lin4_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15_GeographicNames_OnlySpeluncae.recode.vcf.gz --plink --max-missing 0 --out nomissingdata_Lin4_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15
+
+./gone2 
