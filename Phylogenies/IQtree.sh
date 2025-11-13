fad943968d1c38a3bf8672cdeddcdcdbd5c35acf
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-13 18:02:37 -0300

    Update IQtree.sh

--- Phylogenies/IQtree.sh
+++ Phylogenies/IQtree.sh
@@ -5,3 +5,5 @@ python vcf2phylip/vcf2phylip.py -i FilteredPCA_FilteredMax30missingDepthmin5_Geo
 
 # 2. IQ-TREE
 iqtree -s ../vcffiles/FinalVCFs/FilteredPCA_FilteredMax30missingDepthmin5_GeographicNames_allsamples.min4.phy -o Ssuperciliaris -T 5 -b 100 --prefix allsamples_FilteredPCA_FilteredMax30missingDepthmin5_100
+
+iqtree3 -s FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0.15.min4.phy -m MFP --prefix FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0.15 # model finder with a reduced dataset of indidivuals 
