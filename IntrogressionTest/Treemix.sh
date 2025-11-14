b91021507d5602eec11c8c7a4ce0d9f5d457e688
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-14 20:59:31 -0300

    Modify Treemix.sh for clarity and environment setup
    Updated comments and added conda activation for Treemix.

--- IntrogressionTest/Treemix.sh
+++ IntrogressionTest/Treemix.sh
@@ -1,12 +1,12 @@
 # based on https://speciationgenomics.github.io/Treemix/ tutorial
-# vcf to be used: Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz
-# for run 2 use a vcf with novacapitalis as outgroup 
-# bcftools view -s ^Ssuperciliaris,Sdiamantinensis,Spachecoi,Spetrophilus -Oz -o Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05_Snovacapitalusasoutgroup.recode.vcf.gz Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz
-
+conda activate treemix
+# packages to be installed 
+# plink, vcftools, bcftools, 
 # 1. generate clst file from ADMIXTURE file
 bcftools query -l ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz | awk '{split($1,pop,"."); print $1"\t"$1"\t"pop[2]}' > sspeluncae.clust
 
 # 2. do the file conversion of the vcf to spectrum treemix input
+# https://github.com/speciationgenomics/scripts/blob/master/vcf2treemix.sh
 vcf2treemix.sh ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz sspeluncae.clust
 
 # 3. run treemix
