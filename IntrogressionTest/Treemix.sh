7077117199fcf28a76ffc83f64b13c0148a75dd0
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-15 11:23:19 -0300

    Update Treemix.sh

--- IntrogressionTest/Treemix.sh
+++ IntrogressionTest/Treemix.sh
@@ -2,12 +2,15 @@
 conda activate treemix
 # packages to be installed 
 # plink, vcftools, bcftools, 
+# conda activate treemix 
+# python must be version=2
 # 1. generate clst file from ADMIXTURE file
-bcftools query -l ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz | awk '{split($1,pop,"."); print $1"\t"$1"\t"pop[2]}' > sspeluncae.clust
+bcftools query -l *.vcf.gz | awk '{split($1,pop,"."); print $1"\t"$1"\t"pop[2]}' > sspeluncae.clust
 
 # 2. do the file conversion of the vcf to spectrum treemix input
 # https://github.com/speciationgenomics/scripts/blob/master/vcf2treemix.sh
-vcf2treemix.sh ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.vcf.gz sspeluncae.clust
+# the plink2treemix.py must be executable and the oython version is python2
+./vcf2treemix.sh *.vcf.gz sspeluncae.clust
 
 # 3. run treemix
 # specify how many migration edges it should add
@@ -15,5 +18,5 @@ vcf2treemix.sh ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_Filtered
 # conda activate Treemix
 for i in {0..7}
 do
- treemix -i ~/Scytalopus/vcffiles/FinalVCFs/Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.treemix.frq.gz -m $i -o Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.$i -root Ssuperciliaris -bootstrap -k 500 -noss > treemix_${i}_log &
+ treemix -i *.treemix.frq.gz -m $i -o *.$i -root Ssuperciliaris -bootstrap -k 500 -noss > treemix_${i}_log &
 done
