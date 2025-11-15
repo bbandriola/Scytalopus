24b81a6c7733c096d9600f3fda983ad62adabc6b
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-15 15:42:58 -0300

    Update plotTreemixResults.r

--- IntrogressionTest/plotTreemixResults.r
+++ IntrogressionTest/plotTreemixResults.r
@@ -1,10 +1,10 @@
 # from https://speciationgenomics.github.io/Treemix/
 library(RColorBrewer)
 library(R.utils)
-source("plotting_funcs.R")
+source("~/Softwares/Treemix/treemix/src/plotting_funcs.R")
 
-setwd("~/Desktop/Projects/Sspeluncae/Analises/Treemix/") # of course this needs to be adjusted
-prefix="Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05"
+setwd("/media/labgenoma5/DATAPART3/bandriola/Scytalopus/Treemix/NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_superciliarisoutgroup/") # of course this needs to be adjusted
+prefix="Ssuperciliarisoutgroup"
 
 #plot all runs side by side
 par(mfrow=c(2,3))
@@ -21,14 +21,14 @@ for(edge in 1:7){
 ####################################
 ######## with treemix codes ########
 ####################################
-source("~/media/labgenoma4/DATAPART6/bandriola/Softwares/Treemix/treemix/src/plotting_funcs.R")
+source("~/Softwares/Treemix/treemix/src/plotting_funcs.R")
 
 # plot tree
 pdf("mix7.pdf",10,10)
-plot_tree(cex=0.8,"Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.7")
+plot_tree(cex=0.8,"Ssuperciliarisoutgroup.7")
 dev.off()
 
 # plot residual 
 pdf("mix1residual.pdf",10,10)
-plot_resid(stem="Thinned1000_maxmissin0.7_FilteredPCAandUCEandparentes_FilteredMax30missingDepthmin5_GeographicNames_allsamples_maf0.05.recode.1",pop_order="plotorder.txt")
+plot_resid(stem="Ssuperciliarisoutgroup.1",pop_order="plotorder.txt")
 dev.off()
