142e59d44b0a524e03ba360167814ef7f616371b
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-11-17 11:54:13 -0300

    Update f-branch.sh

--- IntrogressionTest/f-branch.sh
+++ IntrogressionTest/f-branch.sh
@@ -2,21 +2,18 @@
 # check https://github.com/mmatschiner/tutorials/blob/master/analysis_of_introgression_with_snp_data/README.md for more
 
 # 1. run Dtrios
-../../Softwares/Dsuite/Build/Dsuite Dtrios -t allsamples_PCAfiltered.nwk -o allsamples_PCAfiltered ../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz popfile.txt
-../../Softwares/Dsuite/Build/Dsuite Dtrios -t Onlyspeluncae_PCAfiltered.nwk -o Onlyspeluncae_PCAfiltered ../vcffiles/FinalVCFs/FilteredMax30missingDepthmin5_GeographicNames_allsamples.vcf.gz Onlyspeluncae_PCAfiltered_popfile.txt
+../../Softwares/Dsuite/Build/Dsuite Dtrios -t speluncaeLin1.nwk -o speluncaeLin1_dtrios ../vcfs/normalizedVCFs/NoZW_FilteredPCAandUCE_Max10missingDepthmin15LD015_GeographicNames_SpeluncaeLin1.recode.vcf.gz speluncaeLin1.txt # running
 
 # 2. Fbranch  
-../../Softwares/Dsuite/Build/Dsuite Fbranch -Z allsamples_PCAfiltered.nwk allsamples_PCAfiltered_tree.txt > fbranch_allsamples_PCAfiltered.txt
-../../Softwares/Dsuite/Build/Dsuite Fbranch -Z Onlyspeluncae_PCAfiltered.nwk Onlyspeluncae_PCAfiltered_tree.txt > fbranch_Onlyspeluncae_PCAfiltered.txt
+../../Softwares/Dsuite/Build/Dsuite Fbranch -Z speluncaeLin1.nwk speluncaeLin1_dtrios_tree.txt > fbranch_speluncaeLin1_dtrios.txt
 
 # 3. fbranch
-../../Softwares/Dsuite/utils/dtools.py -n fbranch_allsamples_PCAfiltered --outgroup Outgroup fbranch_allsamples_PCAfiltered.txt allsamples_PCAfiltered.nwk 
-../../Softwares/Dsuite/utils/dtools.py -n fbranch_Onlyspeluncae_PCAfiltered --outgroup Outgroup fbranch_Onlyspeluncae_PCAfiltered.txt Onlyspeluncae_PCAfiltered.nwk 
+../../Softwares/Dsuite/utils/dtools.py -n fbranch_speluncaeLin1_dtrios --outgroup Outgroup fbranch_speluncaeLin1_dtrios.txt speluncaeLin1.nwk 
 
 # plot D-stats and f4ratio 
 #conda activate dsuite
-ruby plot_f4ratio.rb allsamples_PCAfiltered_BBAA.txt plot_order.txt 0.2 allsamples_PCAfiltered_BBAA_f4ratio.svg
-ruby plot_d.rb allsamples_PCAfiltered_BBAA.txt popfile.txt 0.7 allsamples_PCAfiltered_BBAA_D.svg
+#ruby plot_f4ratio.rb allsamples_PCAfiltered_BBAA.txt plot_order.txt 0.2 allsamples_PCAfiltered_BBAA_f4ratio.svg
+#ruby plot_d.rb allsamples_PCAfiltered_BBAA.txt popfile.txt 0.7 allsamples_PCAfiltered_BBAA_D.svg
 
-ruby plot_f4ratio.rb Onlyspeluncae_PCAfiltered_BBAA.txt Onlyspeluncae_PCAfiltered_plot_order.txt 0.2 Onlyspeluncae_PCAfiltered_BBAA_f4ratio.svg
-ruby plot_d.rb Onlyspeluncae_PCAfiltered_BBAA.txt Onlyspeluncae_PCAfiltered_popfile.txt 0.7 Onlyspeluncae_PCAfiltered_BBAA_D.svg
+#ruby plot_f4ratio.rb Onlyspeluncae_PCAfiltered_BBAA.txt Onlyspeluncae_PCAfiltered_plot_order.txt 0.2 Onlyspeluncae_PCAfiltered_BBAA_f4ratio.svg
+#ruby plot_d.rb Onlyspeluncae_PCAfiltered_BBAA.txt Onlyspeluncae_PCAfiltered_popfile.txt 0.7 Onlyspeluncae_PCAfiltered_BBAA_D.svg
