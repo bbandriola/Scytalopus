e944211abca40a7cf1d21be7fea2f6497d7d2e26
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-12-11 15:23:36 -0300

    Comment out data processing commands in EEMS.R
    Comment out various commands for data processing and plotting in R.

---
+++ IBD&IBE/EEMS.R
@@ -0,0 +1,27 @@
+# 1) vcf to plink file
+#plink --vcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missingDepthmin10maf0.05_GeographicNames_allsamples.vcf.gz --make-bed --out ../snparcher/vcfs/FilteredPCAandUCE_Max30missingDepthmin10maf0.05_GeographicNames_allsamples --allow-extra-chr
+
+# 2) bed2diffs
+#cp datapath.bed newdatapath.bed
+#cp datapath.fam newdatapath.fam
+#awk '{print 1,$2,$3,$4,$5,$6}' datapath.bim  > newdatapath.bim
+
+#./bed2diffs_v1 --bfile FilteredPCAandUCE_Max30missingDepthmin10maf0.05_GeographicNames_speluncaeonly
+
+# 3) run eems
+./runeems_snps --param params-chain1_run*.ini --seed 
+
+#### plot in R
+# https://github.com/dipetkov/eems/blob/master/Documentation/EEMS-doc.pdf
+library("rEEMSplots")
+library("rworldmap")
+library("rworldxtra")
+
+setwd("./") 
+
+mcmcpath = "./FilteredPCAandUCE_Max30missingDepthmin10maf0.05_GeographicNames_speluncaeonly.modified"
+plotpath = "./FilteredPCAandUCE_Max30missingDepthmin10maf0.05_GeographicNames_speluncaeonly.modified"
+
+eems.plots(mcmcpath, plotpath, longlat = TRUE, add.grid=TRUE, add.demes=TRUE,
+          out.png=TRUE,res=800,add.map = TRUE,col.map="black",projection.in = projection_none,
+           projection.out = projection_mercator)
