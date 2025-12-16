7a4fdb0ccf2bb7f21189b10ff163a62691f021ce
bbandriola <78495921+bbandriola@users.noreply.github.com>
2025-12-16 09:18:20 -0300

    Enhance GONE script with comments and plotting
    Updated GONE script with detailed comments and R plotting instructions.

--- Demography/GONE.sh
+++
@@ -1,5 +0,0 @@
-# GONE 
-# 1. generate file ped
-vcftools --gzvcf Lin4_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15_GeographicNames_OnlySpeluncae.recode.vcf.gz --plink --max-missing 0 --out nomissingdata_Lin4_OnlyVariants_NoZW_FilteredPCAandUCE_Max10missingDepthmin15
-
-./gone2 
