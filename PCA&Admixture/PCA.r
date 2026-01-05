bc30588b912c757f26d5f8ddef49c2effc2380c2
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-01-05 10:38:05 -0300

    Update PCA.r

--- PCA&Admixture/PCA.r
+++ PCA&Admixture/PCA.r
@@ -13,20 +13,20 @@ library(RColorBrewer)
 library(tidyr)
 
 # read in data
-pca <- read_table2("Northclade_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.eigenvec", col_names = FALSE)
-eigenval <- scan("Northclade_FilteredMax30missingDepthmin5_mergedVCFproject1_2_5_6_7_8.eigenval")
+pca <- read_table2("Autosomal_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_SspeluncaeOnly.eigenvec", col_names = FALSE)
+eigenval <- scan("Autosomal_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_SspeluncaeOnly.eigenval")
 
 # set names
 # remove ind names and let only lineages 
 pca <- pca[,-1]
-names(pca)[1] <- "lineages"
+names(pca)[1] <- "Lineages"
 names(pca)[2:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
 
 # first convert to percentage variance explained
-pve <- data.frame(PC = 1:13, pve = eigenval/sum(eigenval)*100)
+pve <- data.frame(PC = 1:20, pve = eigenval/sum(eigenval)*100)
 
 # percentage of variance
-pdf("FilteredPCAandUCE_Max30missingDepthmin10LD0.15maf0.05_GeographicNames_speluncaecomplexPCvariance.pdf",width = 10,height = 6)
+pdf("Autosomal_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_SspeluncaeOnly_PCvariance.pdf",width = 10,height = 6)
 a <- ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity")
 a + ylab("Percentage variance explained") + theme_light()
 dev.off()
@@ -35,25 +35,24 @@ dev.off()
 cumsum(pve$pve)
 
 # plot pca circles
-pdf("FilteredPCAandUCE_Max30missingDepthmin10LD0.15maf0.05_GeographicNames_speluncaecomplex_PCA.pdf",width = 10,height = 10)
-b <- ggplot(pca, aes(PC1, PC2, color = lineages)) + 
+pdf("Autosomal_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_SspeluncaeOnly_PCA.pdf",width = 10,height = 10)
+b <- ggplot(pca, aes(PC1, PC2, color = Lineages)) + 
   geom_point(position = position_jitter(seed = 1, width = 0.02, height=0.03),size = 6)+
-  scale_color_manual(values = c("#0660FB","#4D9F8C","#7570B3","deeppink","#66A61D","#E93F33","#E6AC2F"))+ 
-  #scale_color_viridis_d(option = "D")  + 
+  scale_color_manual(values = c("#0660FB","#4D9F8C","#7570B3","deeppink","#66A61D","#E93F33","#E6AC2F"))+
   coord_equal() + 
-  theme_bw(base_size = 20) +
-  theme (axis.ticks = element_line(colour = "white", size = 0.3),
-         panel.grid.major = element_line(colour = "white", size = 0.3),
-         panel.grid.minor = element_blank()
-  )
+  theme_bw(base_size = 15) +
+  theme(axis.title = element_text(size = 15),
+    axis.title.x = element_text(size =15),  
+    axis.title.y = element_text(size =15))+
+    labs(fill = "Lineage")+
+    theme(axis.ticks = element_line(colour = "gray60", size = 0.2),
+    panel.grid.major = element_line(colour = "white", size = 0.2),
+    panel.border = element_rect(colour = "gray60", fill = NA))
 b + xlab(paste0("PC1(", signif(pve$pve[1], 3), "%)")) + ylab(paste0("PC2(", signif(pve$pve[2], 3), "%)"))+
   labs(color = "Lineage")+
-  theme_bw(base_size = 20)+
-  theme(#axis.title = element_text(size = 20),
-    axis.title.x = element_text(size =20),  
-    axis.title.y = element_text(size =20))+
+  theme(axis.title.x = element_text(size =15),  
+    axis.title.y = element_text(size =15))+
     labs(fill = "Lineage")
-
 dev.off()
 
 ###############################################
