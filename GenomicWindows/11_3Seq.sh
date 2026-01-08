8ff053465e13d99db74b75a3be7dc31bed60c4f0
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-01-08 13:55:43 -0300

    Update output file name for Bonferroni results

--- GenomicWindows/11_3Seq.sh
+++ GenomicWindows/11_3Seq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #run 3 seq
-for i in edited*.fa; do /media/labgenoma4/DATAPART6/bandriola/Softwares/3Seq/3seq\ build\ 170612/3seq -f $i -id $i; done
+for i in edited*.fa; do ~/Softwares/3Seq/3seq\ build\ 170612/3seq -f $i -id $i; done
 
 echo "3seq done"
 
@@ -20,7 +20,7 @@ echo "moved files"
 
 cd ./3seq/
 
-for i in *.3s.log; do grep -H 'Bonferroni' $i >> Bonferroni_5kb.csv; done
+for i in *.3s.log; do grep -H 'Bonferroni' $i >> Bonferroni_1kb.csv; done
 
 #open the file on excel and check for recombination into the window <cut at 0,01>
 
