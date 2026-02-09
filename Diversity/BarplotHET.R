a6c26e01713383621f55e07900e0297508ea5653
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-02-08 21:57:39 -0300

    Modify Heterozigosidade label format and lineage colors
    Updated the label formatting for Heterozigosidade in the bar plot and adjusted the color mapping for lineages.

--- Diversity/BarplotHET.R
+++ Diversity/BarplotHET.R
@@ -6,10 +6,10 @@ df$SampleID <- factor(df$SampleID, levels = rev(levels(df$SampleID)))
 
 p<-ggplot(df, aes(x = SampleID, y = Heterozigosidade, fill = Lineage)) +
   geom_bar(stat = "identity", width = 0.8) +
-  geom_text(aes(label = sprintf("%.4f", Heterozigosidade)),hjust = 1.1,color = "white",size = 2.8)+
+  geom_text(aes(label = sprintf("%.6f", Heterozigosidade)),hjust = 1.1,color = "white",size = 2.5)+
   labs(x = NULL,y = "Mean Heterozygosity",fill = "Lineage") +
-  scale_fill_manual(values = c("Lin1" = "#0660FB","Lin2"="#4D9F8C","Lin3" = "#7570B3","Lin4" = "deeppink","Lin5" = "#66A61D",
-    "Lin6" = "#E93F33","Lin7" = "#E6AC2F"))+
+  scale_fill_manual(values = c("lin1" = "#0660FB","lin2"="#4D9F8C","lin3" = "#7570B3","lin4" = "deeppink","lin5" = "#66A61D",
+    "lin6" = "#E93F33","lin7" = "#E6AC2F"))+
   theme_minimal(base_size = 10) +
   theme(axis.title = element_text(size = 10),
     axis.title.x = element_text(size =10),  
