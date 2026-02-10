ec7bd78c805fa60ad747c4873f1c505e824ba59e
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-02-10 14:35:13 -0300

    Update and rename TajimasD.sh to TajimasD.R

--- Diversity/TajimasD.sh
+++
@@ -1,8 +0,0 @@
-# Tajima estimation 
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin1_10kb --keep Lin1.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin2_10kb --keep Lin2.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin3_10kb --keep Lin3.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin4_10kb --keep Lin4.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin57SPPR_10kb --keep Lin57SPPR.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out LinBoaEsperanca67_10kb --keep LinBoaEsperanca67.txt
-vcftools --gzvcf ../snparcher/vcfs/FilteredPCAandUCE_Max30missinessDepthmin10max100_Sspeluncaecomplex.vcf.gz --TajimaD 10000 --out Lin7SCRS_10kb --keep Lin7SCRS.txt
