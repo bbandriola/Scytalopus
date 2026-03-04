170e608635df889c546e079a5dd4f13e5e337e6f
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-03-04 17:07:51 -0300

    Update output filename formatting in step3_filteredBam.sh

--- Demography/Stairway-plot/step3_filteredBam.sh
+++ Demography/Stairway-plot/step3_filteredBam.sh
@@ -1,1 +1,1 @@
-for i in *_lin1_final.bam; do samtools view -b -L Lin1_common_regions.bed -o $i_FilteredRegions20and60.bam $i; done
+for i in *_lin1_final.bam; do samtools view -b -L Lin1_common_regions.bed -o "${i%.bam}_FilteredRegions20and60.bam" "$i"; done
