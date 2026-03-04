9f7bdc3dcdf78ce7e7ccd573dd8ab3f8e2ca06dd
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-03-04 17:15:25 -0300

    Add step4_createsaf.sh for SAF file creation

---
+++ Demography/Stairway-plot/step4_createsaf.sh
@@ -0,0 +1,1 @@
+angsd -b filteredbamlist_Lin1.txt -out Lin1_FilteredRegions20and60depth -gl 1 -dosaf 1 -uniqueOnly -minQ 30 -nThreads 10 -anc ~/../../DATAPART7/sandro_grupo/raw_data/bruna/refSsuperciliaris/GCA_013400415.1_ASM1340041v1/GCA_013400415.1_ASM1340041v1_genomic.fa
