68ec5f679ddc15c402a218298cb7f205f7d265d9
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-05-18 14:32:52 -0300

    Update IQtree.sh

--- Phylogenies/IQtree.sh
+++ Phylogenies/IQtree.sh
@@ -10,3 +10,10 @@ iqtree3 -s FilteredInd_FilteredPCAandUCE_Max30missingDepthmin10LD0.15.min4.phy -
 
 # Concordant Factor 
 iqtree2 -t ../species_tree --gcf ../../RaxmlHPC/50kb/allbestTrees_50kb.fa -p ../../windows/windows_50kb/50kb_allsamples/8_2_LIST_FILE_50kb/windows/Edited_fastas --scf 100 --prefix conca_sCF_gCF.SRIextern -T 10
+
+# concordance vector 
+java -jar ~/Softwares/astral/ASTRAL/astral.5.7.8.jar -i all_trees_1kb_10ind.newick --output 1kb_int25kb.10ind_annotated.tree -t 2 --outgroup Sspeluncae120
+# quartest are writing as q1,q2 and q3
+
+# get the branch lengths in coalescent units from the ASTRAL analysis
+iqtree3 -te 1kb_int25kb.10ind_annotated.tree -blfix -p loci.best_model.nex --scfl 1 --prefix coalescent_bl -T 128
