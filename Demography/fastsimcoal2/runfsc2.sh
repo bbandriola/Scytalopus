4d8733094283ed932191ca8670017597507a5d07
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-06-05 09:51:48 -0300

    Add runfsc2.sh script with model descriptions

---
+++ Demography/fastsimcoal2/runfsc2.sh
@@ -0,0 +1,5 @@
+# to run the four models 
+# model1 -> topology (LinDEV,(Lin3,Lin4)) no introgression
+# model2 -> topology (Lin3,(LinDEV,Lin4)) no introgression
+# model3 -> topology (LinDEV,(Lin3,Lin4)) with introgression from DEV -> Lin4
+# model4 -> topology (Lin3,(LinDEV,Lin4)) with introgression from Lin3 -> Lin4
