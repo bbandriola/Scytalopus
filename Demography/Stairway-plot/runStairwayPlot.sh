2878d9d64b59e2bd260e5f584c4ba0efa950497d
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-06-12 09:49:13 -0300

    Enhance runStairwayPlot.sh with additional commands
    Added commands to run Stairbuilder for multiple blueprints and execute related scripts.

--- Demography/Stairway-plot/runStairwayPlot.sh
+++ Demography/Stairway-plot/runStairwayPlot.sh
@@ -1,1 +1,12 @@
 # https://github.com/xiaoming-liu/stairway-plot-v2
+
+java -cp stairway_plot_es Stairbuilder Lin1_unfolded.blueprint
+java -cp stairway_plot_es Stairbuilder Lin3Mant_unfolded.blueprint
+java -cp stairway_plot_es Stairbuilder Lin4_unfolded.blueprint
+java -cp stairway_plot_es Stairbuilder Lin5_unfolded.blueprint
+java -cp stairway_plot_es Stairbuilder Lin6_unfolded.blueprint
+java -cp stairway_plot_es Stairbuilder Lin7_unfolded.blueprint
+
+bash Lin7_unfolded.blueprint.sh 
+
+bash Lin7_unfolded.blueprint.plot.sh
