6526c5738c6730091ad5158b2627ca9a81c84580
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-06-18 10:15:16 -0300

    Update plotPARdistribution.r

--- Demography/fastsimcoal2/plotPARdistribution.r
+++ Demography/fastsimcoal2/plotPARdistribution.r
@@ -6,3 +6,10 @@ pdf("dist_MaxEstLhood.pdf", height = 5, width = 6)
 ggplot(table)+geom_line(aes(y=RUN,x=MaxEstLhood))
 dev.off()
 
+ggplot(table, aes(x = RUN, y = MaxEstLhood)) + geom_line() +
+theme_bw() +
+
+scale_y_continuous(breaks = unique(table$MaxEstLhood)
+ggplot(table, aes(x = RUN, y = TSPLIT2)) + geom_line() +
+theme_bw() +
+scale_y_continuous(breaks = unique(table$TSPLIT2))
