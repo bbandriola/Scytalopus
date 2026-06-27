69a65a30bdfb9bd1db79d1406b0cb2b5d3211f73
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-06-27 14:13:34 -0300

    Update Treemix.sh

--- IntrogressionTest/Treemix.sh
+++ IntrogressionTest/Treemix.sh
@@ -11,12 +11,20 @@ bcftools query -l *.vcf.gz | awk '{split($1,pop,"."); print $1"\t"$1"\t"pop[2]}'
 # https://github.com/speciationgenomics/scripts/blob/master/vcf2treemix.sh
 # the plink2treemix.py must be executable and the oython version is python2
 ./vcf2treemix.sh *.vcf.gz sspeluncae.clust
+# had to stop when the command ask for the python script and change for the python 2 environment, than went back to the treemix env and kept to the rest of the script
 
 # 3. run treemix
 # specify how many migration edges it should add
 # run from 0 - 7, setting Ssuperciliaris as outgroup 
 # conda activate Treemix
-for i in {0..7}
-do
- treemix -i *.treemix.frq.gz -m $i -o *.$i -root Ssuperciliaris -bootstrap -k 500 -noss > treemix_${i}_log &
+for m in {0..7}; do
+  for rep in {1..3}; do
+    treemix \
+      -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.recode.treemix.frq.gz \
+      -o SnovacapitalisOutgroup${m}_rep${rep} \
+      -m $m \
+      -k 500 \
+      -noss \
+      -root Snovacapitalis
+  done
 done
