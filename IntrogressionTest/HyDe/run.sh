829087ef3f6b7553597162027c3a69846c61bb3d
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-07-01 09:59:46 -0300

    Update run.sh

--- IntrogressionTest/HyDe/run.sh
+++ IntrogressionTest/HyDe/run.sh
@@ -1,13 +1,11 @@
 # 1. convert vcf to phylip
-python ../outgroup_Snovacap/vcf_to_phylipIUPACode.py NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.recode.vcf.gz NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.phy
+python3 ~/Softwares/vcf_to_phylipIUPACode.py FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.recode.vcf.gz FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy
 
 # 2. Run HyDe
-python run_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.phy -m map.txt -o Lin1 -n 54 -t 7 -s 1636679 -q --prefix SpeluncaeLin1
-python run_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1Snovacap.phy -m map.txt -o out -n 55 -t 8 -s 1801056 -q --prefix SpeluncaeLin1Snovacap
+python run_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -o out -n 54 -t 15 -s 1636679 -q --prefix complex_novacapout
 
 # 3. testing for the dynamic of the introgression between individuals (to understand if is uniform or not)
 # it needs the input of the triplets. So we would test only the populations that seemed introgressed 
 # the triple file can be the output of the run_hyde.py run
-python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1Snovacap.phy -m map.txt -tr triplets2test.txt -o out -n 55 -t 8 -s 1801056
-python individual_hyde.py -i NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_SpeluncaeLin1.phy -m map.txt -tr triplets2test.txt -o Lin1 -n 54 -t 7 -s 1636679 --prefix Individualtest_SpeluncaeLin1 
+python individual_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -tr triplets2test.txt -o out -n 55 -t 8 -s 1801056 --prefix Individualtest_
 
