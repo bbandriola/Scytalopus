e260ec1a1142e58e0566374f39749ae98bfa9c3b
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-07-13 16:58:10 -0300

    Comment out unused commands in WholeGenomeSNPsTrees.sh
    Comment out IQ-TREE and CASTER commands in the script.

--- Phylogenies/WholeGenomeSNPsTrees.sh
+++ Phylogenies/WholeGenomeSNPsTrees.sh
@@ -4,17 +4,14 @@
 # https://github.com/edgardomortiz/vcf2phylip
 python vcf2phylip.py --input file.vcf.gz --output-prefix file
 
-# IQ-TREE: 
-iqtree -s file.phy --alrt 1000 -B 1000 -b 100 -o Snovacapitalis152_lin --prefix fileprefix_IQtree -T 5
-
 # RAxML-NG:
-raxml-ng-mpi --msa file.phy --all --prefix fileprefix_raxml --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
+# raxml-ng-mpi --msa file.phy --all --prefix fileprefix_raxml --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
 
 raxml-ng-mpi --msa ./Zscaffold_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_allsamples.recode.min4.phy --all --prefix Zscaffold_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_allsamples --threads 5 --model GTR --outgroup Ssuperciliaris --bs-trees 100
 raxml-ng-mpi --msa ../../../../DATAPART3/bandriola/Scytalopus/snparcher/vcfs/NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.min4.phy --all --prefix NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples --threads 6 --model GTR --outgroup Snovacapitalis --bs-trees 100
 
 # CASTER:
-../../Softwares/ASTER/bin/caster-site --output file.caster.newick --thread 5 --support 1 --input file.phy 2> allsamples.caster.log
+#../../Softwares/ASTER/bin/caster-site --output file.caster.newick --thread 5 --support 1 --input file.phy 2> allsamples.caster.log
 
 # NJ with VCF2Dis
 # https://github.com/hewm2008/VCF2Dis
