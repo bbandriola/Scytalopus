6723007d8d217b5a32c6aed5cb26b9045caf186e
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-07-13 21:51:01 -0300

    Update WholeGenomeSNPsTrees.sh

--- Phylogenies/WholeGenomeSNPsTrees.sh
+++ Phylogenies/WholeGenomeSNPsTrees.sh
@@ -7,8 +7,8 @@ python vcf2phylip.py --input file.vcf.gz --output-prefix file
 # RAxML-NG:
 # raxml-ng-mpi --msa file.phy --all --prefix fileprefix_raxml --threads 6 --model GTR --outgroup Snovacapitalis152_lin --bs-trees 100
 
-raxml-ng-mpi --msa ./Zscaffold_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_allsamples.recode.min4.phy --all --prefix Zscaffold_FilteredPCAandUCE_Max30missinessDepthmin10max100LD0.15_allsamples --threads 5 --model GTR --outgroup Ssuperciliaris --bs-trees 100
-raxml-ng-mpi --msa ../../../../DATAPART3/bandriola/Scytalopus/snparcher/vcfs/NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples.min4.phy --all --prefix NoZW_FilteredPCAandUCE_Max30missingDepthmin10LD015_GeographicNames_allsamples --threads 6 --model GTR --outgroup Snovacapitalis --bs-trees 100
+raxml-ng-mpi --msa ./Zscaffolds_FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_allsamples.min4.phy --all --prefix ../../../RAxML_WGTrees/Zscaffolds_FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_allsamples --threads 5 --model GTR --outgroup Ssuperciliaris --bs-trees 100
+raxml-ng-mpi --msa ./NoZW_FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_allsamples.min4.phy --all --prefix ../../../RAxML_WGTrees/NoZW_FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_allsamples --threads 6 --model GTR --outgroup Ssuperciliaris --bs-trees 100
 
 # CASTER:
 #../../Softwares/ASTER/bin/caster-site --output file.caster.newick --thread 5 --support 1 --input file.phy 2> allsamples.caster.log
