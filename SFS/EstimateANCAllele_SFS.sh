769c1cc25b61750017392bf0380288684dc8998a
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-07-30 12:57:32 -0300

    Update EstimateANCAllele_SFS.sh

--- SFS/EstimateANCAllele_SFS.sh
+++ SFS/EstimateANCAllele_SFS.sh
@@ -152,10 +152,9 @@ while read bam; do echo "Reindexing $bam"; samtools index -@ 10 "$bam"; done < L
 while read bam; do echo "Reindexing $bam"; samtools index -@ 10 "$bam"; done < Lin3_OnlyMant.bamlist
 while read bam; do echo "Reindexing $bam"; samtools index -@ 10 "$bam"; done < Lin1.bamlist
 
-
 # SFS unfolded for all other lineages
 angsd -GL 1 -b Lin5.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin5 -doSaf 1 -minInd 4 -rf chr2include.txt
-angsd -GL 1 -b Lin6.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin6 -doSaf 1 -minInd 4 -rf chr2include.txt
+angsd -GL 1 -b Lin6.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin6 -doSaf 1 -rf chr2include.txt
 angsd -GL 1 -b Lin7.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin7 -doSaf 1 -minInd 4 -rf chr2include.txt
 angsd -GL 1 -b Lin3_OnlyMant.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin3_OnlyMant -doSaf 1 -minInd 4 -rf chr2include.txt
 angsd -GL 1 -b Lin1.bamlist -anc editedfastaancallele/editedACGT_subset41chr_allNchr.fasta -P 10 -out Lin1 -doSaf 1 -minInd 4 -rf chr2include.txt
@@ -165,14 +164,3 @@ realSFS Lin5.saf.idx -P 10 > Lin5_dsfs.sfs
 realSFS Lin6.saf.idx -P 10 > Lin6_dsfs.sfs
 realSFS Lin7.saf.idx -P 10 > Lin7_dsfs.sfs
 realSFS Lin1.saf.idx -P 10 > Lin1_dsfs.sfs
-
-
-
-
-
-
-
-
-
-
-
