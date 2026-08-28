2be76ad1916c5254e7c70f19e933153f8a3d6eaf
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-08-28 10:01:32 -0300

    Update run.sh

--- IntrogressionTest/HyDe/run.sh
+++ IntrogressionTest/HyDe/run.sh
@@ -2,10 +2,12 @@
 python3 ~/Softwares/vcf_to_phylipIUPACode.py FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.recode.vcf.gz FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy
 
 # 2. Run HyDe
-python run_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -o out -n 54 -t 15 -s 1636679 -q --prefix complex_novacapout
+python run_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -o out -n 55 -t 9 -s 1577994 -q --prefix complex_novacapout
 
 # 3. testing for the dynamic of the introgression between individuals (to understand if is uniform or not)
 # it needs the input of the triplets. So we would test only the populations that seemed introgressed 
 # the triple file can be the output of the run_hyde.py run
-python individual_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -tr triplets2test.txt -o out -n 55 -t 8 -s 1801056 --prefix Individualtest_
+python individual_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -tr triplets2test_linnorth4south.txt -o out -n 55 -t 9 -s 1577994 --prefix Individualtest_
 
+# 4. bootstrap 
+python ~/Softwares/HyDe/scripts/bootstrap_hyde.py -i FilteredLDfiltered_MinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_complex_novacapitalis.phy -m map_separaringDevoniana.txt -tr triplets2test_linnorth4south.txt -o out -n 55 -t 9 -s 1577994 --prefix boots_individualsnorthlin4south
