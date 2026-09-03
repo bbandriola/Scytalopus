1362dd98a9a5947640c038f061c6d16a30560607
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-09-03 14:10:09 -0300

    Create msci.sh

---
+++ Phylogenies/msci.sh
@@ -0,0 +1,7 @@
+# run BPP
+
+# to choose the model
+~/Software/bpp-4.8.7-linux-x86_64/bin/bpp --bfdriver BPP_introgression2to4first.ctl --points 4
+~/Software/bpp-4.8.7-linux-x86_64/bin/bpp --bfdriver BPP_introgression5to4first.ctl --points 4
+
+# run the models
